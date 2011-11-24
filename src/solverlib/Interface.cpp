900a18aa6ad2d6bebe391f72921f9a67c165af4d
Roberto Di Remigio <roberto.diremigio@gmail.com>
2011-11-24 12:04:26 +0100

    Working on interface.

--- src/solverlib/Interface.cpp
+++ src/solverlib/Interface.cpp
@@ -1,10 +1,24 @@
 /*
 
-Interface routines implementation
+Interface functions implementation
 
 */
 
+#include <iostream>
+#include <iomanip>
+#include <fstream> 
+#include <string>
 
+#include <Eigen/Dense>
+
+#include "Getkw.h"
+#include "Cavity.h"
+#include "GePolCavity.h"
+#include "GreensFunction.h"
+#include "Vacuum.h"
+#include "UniformDielectric.h"
+#include "PCMSolver.h"
+#include "IEFSolver.h"
 #include "Interface.h"
 
 using namespace std;
@@ -12,6 +26,53 @@ using namespace Eigen;
 
 GePolCavity *cavity;
 IEFSolver *solver;
+
+/*
+
+  Cavity related functions
+
+*/
+
+extern "C" void init_gepol_cavity_() {
+	const char *infile = 0;
+	infile = "@pcmsolver.inp";
+	Getkw Input = Getkw(infile, false, true);
+	cavity = new GePolCavity(Input, "Cavity<gepol>");
+	if ( Input.getStr("Cavity<gepol>.Mode") == "Atoms" ){
+	  vector<int> atomsInput = Input.getIntVec("Cavity<gepol>.Atoms");
+	  vector<double> radiiInput = Input.getDblVec("Cavity<gepol>.Radii");
+	  init_atoms_(cavity->getNSpheres(), atomsInput, cavity->getSphereCenter());
+	}
+	VectorXd rob1;
+	Matrix<double, 3, Dynamic> rob2;
+	init_implicit_(rob1, rob2);
+	//	cout << cavity->getSphereCenter() << endl;
+	cavity->makeCavity(5000, 10000000);
+	cavity->initPotChg();
+}
+
+extern "C" void init_atoms_(int nSpheres, vector<int> & atomsInput, 
+			    Matrix<double, 3, Dynamic> & sphereCenter){
+  double * centers = sphereCenter.data();
+  int * idx = new int[nSpheres];
+  for ( int i = 0; i < nSpheres; i++ ){
+    idx[i] = atomsInput[i];
+  }
+  collect_atoms_(& nSpheres, idx, centers);
+  //  cout << sphereCenter << endl;
+ }
+
+extern "C" void init_implicit_(VectorXd & chg, Matrix<double, 3, Dynamic> & sphereCenter){
+  int nuclei;
+  collect_nctot_(&nuclei);
+  chg.resize(nuclei);
+  sphereCenter.resize(NoChange, nuclei);
+  double * charges = chg.data();
+  double * centers = sphereCenter.data();
+  collect_implicit_(charges, centers);
+  cout << chg << endl;
+  cout << sphereCenter << endl;
+}
 
 extern "C" void get_cavity_size_(int * nts) {
 	*nts = cavity->size();
@@ -53,12 +114,12 @@ extern "C" void comp_pot_chg_pcm_(double *density, double *work, int *lwork) {
 	nuc_pot_pcm_(cavity->getTessCenter().data(), &nts, 
 				 cavity->getPot(Cavity::Nuclear).data());
 	ele_pot_pcm_(density, cavity->getTessCenter().data(), &nts, 
-				 cavity->getPot(Cavity::Electronic).data(), work, lwork);
+		     cavity->getPot(Cavity::Electronic).data(), work, lwork);
 
 	solver->compCharge(cavity->getPot(Cavity::Nuclear),    
-					   cavity->getChg(Cavity::Nuclear));
+			   cavity->getChg(Cavity::Nuclear));
 	solver->compCharge(cavity->getPot(Cavity::Electronic), 
-					   cavity->getChg(Cavity::Electronic));
+			   cavity->getChg(Cavity::Electronic));
 
 	double totElChg = cavity->getChg(Cavity::Electronic).sum();
 	double totNuChg = cavity->getChg(Cavity::Nuclear).sum();
@@ -67,6 +128,17 @@ extern "C" void comp_pot_chg_pcm_(double *density, double *work, int *lwork) {
 extern "C" void comp_pol_ene_pcm_(double * energy) {
 	* energy = cavity->compPolarizationEnergy();
 }
+
+extern "C" void print_gepol_cavity_(){
+	cout << "Cavity size" << cavity->size() << endl;
+}
+
+/*
+
+  Solver related functions
+
+*/
+
 
 extern "C" void init_pcm_() {
 	const char *infile = 0;
@@ -81,21 +153,6 @@ extern "C" void init_pcm_() {
 	} 
 }
 
-extern "C" void init_gepol_cavity_() {
-	const char *infile = 0;
-	infile = "@pcmsolver.inp";
-	Getkw Input = Getkw(infile, false, true);
-	cavity = new GePolCavity(Input, "Cavity<gepol>");
-	if ( Input.getStr("Cavity<gepol>.Mode") == "Atoms" ){
-	  vector<int> atomsInput = Input.getIntVec("Cavity<gepol>.Atoms");
-	  vector<double> radiiInput = Input.getDblVec("Cavity<gepol>.Radii");
-	  init_atoms_(cavity->getNSpheres(), atomsInput, cavity->getSphereCenter());
-	}
-	//	cout << cavity->getSphereCenter() << endl;
-	cavity->makeCavity(5000, 10000000);
-	cavity->initPotChg();
-}
-
 extern "C" void init_pcmsolver_() {
 	const char *infile = 0;
 	infile = "@pcmsolver.inp";
@@ -105,12 +162,12 @@ extern "C" void init_pcmsolver_() {
 	solver->buildIsotropicMatrix(*cavity);
 }
 
-extern "C" void build_anisotropic_matrix_() {
-	solver->buildAnisotropicMatrix(*cavity);
-}
-
 extern "C" void build_isotropic_matrix_() {
 	solver->buildIsotropicMatrix(*cavity);
+}
+
+extern "C" void build_anisotropic_matrix_() {
+	solver->buildAnisotropicMatrix(*cavity);
 }
 
 //copying mechanism of the following routine needs to be revised
@@ -127,20 +184,4 @@ extern "C" void comp_charge_(double *potential_, double *charge_) {
 	}
 }
 
-extern "C" void print_gepol_cavity_(){
-	cout << "Cavity size" << cavity->size() << endl;
-}
 
-extern "C" void init_atoms_(int nSpheres, vector<int> & atomsInput, 
-			       Matrix<double, 3, Dynamic> & sphereCenter){
-  double * coord = sphereCenter.data();
-    int * idx = new int[nSpheres];
-  for ( int i = 0; i < nSpheres; i++ ){
-    idx[i] = atomsInput[i];
-  }
-  collect_atoms_(& nSpheres, idx, coord);
-  cout << sphereCenter << endl;
- }
-
-extern "C" void init_implicit_(Matrix<double, 3, Dynamic> & sphereCenter){
-}
--- src/solverlib/Interface.h
+++ src/solverlib/Interface.h
@@ -1,40 +1,31 @@
+#ifndef INTERFACE_H_
+#define INTERFACE_H_
 /*
 
-Interface routines prototypes
+  Interface functions prototypes.
 
 */
 
 
-#include <iostream>
-#include <iomanip>
-#include <fstream> 
-#include <string>
-
-#include <Eigen/Dense>
-
-#include "Getkw.h"
-#include "Cavity.h"
-#include "GePolCavity.h"
-#include "GreensFunction.h"
-#include "Vacuum.h"
-#include "UniformDielectric.h"
-#include "PCMSolver.h"
-#include "IEFSolver.h"
-
 /*
 
-  Cavity related routines
+  Cavity related functions
 
 */
+
+extern "C" void init_gepol_cavity_();
+
+extern "C" void collect_nctot_(int * nuclei);
 
 extern "C" void collect_atoms_(int * nSpheres, int * idx, double * centers);
+
+extern "C" void collect_implicit_(double * charges, double * centers);
 
 extern "C" void init_atoms_(int nSpheres, vector<int> & atomsInput, 
 			    Matrix<double, 3, Dynamic> & sphereCenter);
 
-extern "C" void collect_implicit_(double * centers);
-
-extern "C" void init_implicit_(Matrix<double, 3, Dynamic> & sphereCenter);
+extern "C" void init_implicit_(VectorXd & charges, 
+			       Matrix<double, 3, Dynamic> & sphereCenter);
 
 extern "C" void get_cavity_size_(int * nts);
 
@@ -50,46 +41,37 @@ extern "C" void comp_pot_chg_pcm_(double *density, double *work, int *lwork);
 
 extern "C" void comp_pol_ene_pcm_(double * energy);
 
-extern "C" void init_gepol_cavity_();
-
 extern "C" void print_gepol_cavity_();
 
 
 /*
 
-  Solver related routines
+  Solver related functions
 
  */
+
+extern "C" void init_pcm_();
+
+extern "C" void init_pcmsolver_();
+
+extern "C" void build_isotropic_matrix_();
+
+extern "C" void build_anisotropic_matrix_();
+
+//copying mechanism of the following routine needs to be revised
+extern "C" void comp_charge_(double *potential_, double *charge_);
 
 //      Subroutine PotExpVal(Density, Centers, Nts, Potential, Work, 
 //     $                     LWork)
 extern "C" void ele_pot_pcm_(double *density, double* centers, int *nts, 
-							  double *potential, double *work, int *lwork);
+			     double *potential, double *work, int *lwork);
 
 extern "C" void nuc_pot_pcm_(double* centers, int *nts, double *potential);
 
 //      Subroutine Fock_PCMModule(Fock, Centers, Nts, Charges, Work, 
 //     $                     LWork)
 extern "C" void fock_pcm_(double *fock, double* centers, int *nts, 
-								 double *charges, double *work, int *lwork);
+			  double *charges, double *work, int *lwork);
 
-
-extern "C" void init_pcmsolver_();
-
-extern "C" void init_pcm_();
-
-
-extern "C" void init_pcm_();
-
-
-
-extern "C" void init_pcmsolver_();
-
-extern "C" void build_anisotropic_matrix_();
-
-extern "C" void build_isotropic_matrix_();
-
-//copying mechanism of the following routine needs to be revised
-extern "C" void comp_charge_(double *potential_, double *charge_);
-
+#endif
 

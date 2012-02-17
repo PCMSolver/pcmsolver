f8a05a9c567d59c75aa7bae164f649a29a7675da
Roberto Di Remigio <roberto.diremigio@gmail.com>
2012-02-17 17:11:23 +0100

    Commented out a function.

--- src/solverlib/Interface.cpp
+++ src/solverlib/Interface.cpp
@@ -164,7 +164,7 @@ extern "C" void get_tess_centers_(double * centers) {
 	
 }
 
-extern "C" void comp_pot_chg_pcm_(double *density, double *work, int *lwork) {
+/*extern "C" void comp_pot_chg_pcm_(double *density, double *work, int *lwork) {
 	int nts = _cavity->size();
 	nuc_pot_pcm_(_cavity->getTessCenter().data(), &nts, 
 				 _cavity->getPot(Cavity::Nuclear).data());
@@ -178,7 +178,7 @@ extern "C" void comp_pot_chg_pcm_(double *density, double *work, int *lwork) {
 
 	double totElChg = _cavity->getChg(Cavity::Electronic).sum();
 	double totNuChg = _cavity->getChg(Cavity::Nuclear).sum();
-}
+}*/
 
 extern "C" void comp_pol_ene_pcm_(double * energy) {
 	* energy = _cavity->compPolarizationEnergy();

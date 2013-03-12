471cdc749eaf063f653a8312a0cadc4f29980652
Roberto Di Remigio <roberto.diremigio@gmail.com>
2013-03-12 15:23:51 +0100

    Update BLAS and LAPACK modules of cmake. We now require at least Eigen 3.0.0

--- cmake/MathLibFunctions.cmake
+++ cmake/MathLibFunctions.cmake
@@ -14,88 +14,110 @@
 #  License text for the above reference.)
 
 include(FindPackageHandleStandardArgs)
+include(FindPackageMessage)
 
 if (NOT MATH_LANG)
-	set(MATH_LANG C)
+    set(MATH_LANG C)
 elseif(MATH_LANG STREQUAL "C" OR MATH_LANG STREQUAL "CXX")
-	set(MATH_LANG C)
+    set(MATH_LANG C)
 elseif(NOT MATH_LANG STREQUAL "Fortran")
-	message(FATAL_ERROR "Invalid math library linker language: ${MATH_LANG}")
+    message(FATAL_ERROR "Invalid math library linker language: ${MATH_LANG}")
 endif()
 
-macro(find_math_header _service)
-	string(TOUPPER ${_service} _SERVICE)
-	if (${_service}_h)
-		find_path(${_service}_include_dirs
-			NAMES ${${_service}_h}
-			PATHS ${${_SERVICE}_ROOT}
-			PATH_SUFFIXES include ${path_suffixes}
-			NO_DEFAULT_PATH
-			)
-		find_path(${_service}_include_dirs
-			NAMES ${${_service}_h}
-			PATH_SUFFIXES include
-			)
-	endif()
+macro(find_math_header _service _header)
+    string(TOUPPER ${_service} _SERVICE)
+    find_path(${_SERVICE}_INCLUDE_DIRS
+        NAMES ${_header}
+        PATHS ${${_SERVICE}_ROOT}
+        HINTS ${${_SERVICE}_ROOT}/include
+        PATH_SUFFIXES ${MATH_INCLUDE_PATH_SUFFIXES}
+        NO_DEFAULT_PATH
+        )
+    find_path(${_SERVICE}_INCLUDE_DIRS
+        NAMES ${_header}
+        PATH_SUFFIXES include
+        )
+    set (${_SERVICE}_H ${_header})
+    unset (_SERVICE)
 endmacro()
 
 macro(find_math_libs _service)
-	string(TOUPPER ${_service} _SERVICE)
-	foreach(${_service}lib ${${_service}_libs})
-		find_library(_lib ${${_service}lib}
-			PATHS ${${_SERVICE}_ROOT}
-			PATH_SUFFIXES ${path_suffixes}
-			NO_DEFAULT_PATH
-			)
-		find_library(_lib ${${_service}lib}
-			PATH_SUFFIXES ${path_suffixes}
-			)
-		if(_lib)
-			set(${_service}_libraries ${${_service}_libraries} ${_lib})
-			unset(_lib CACHE)
-		else()
-			break()
-		endif()
-	endforeach()
-	unset(${_service}lib)
-	unset(_lib CACHE)
+    string(TOUPPER ${_service} _SERVICE)
+    if(${_SERVICE}_FOUND)
+        return()
+    endif()
+    set(_lib)
+    set(_libs)
+    foreach(l ${ARGN})
+        find_library(_lib
+            NAMES ${l}
+            PATHS ${${_SERVICE}_ROOT}
+            HINTS ${${_SERVICE}_ROOT}/lib64 ${${_SERVICE}_ROOT}/lib
+            PATH_SUFFIXES ${MATH_LIBRARY_PATH_SUFFIXES}
+            NO_DEFAULT_PATH
+            )
+        find_library(_lib
+            NAMES ${l}
+            PATH_SUFFIXES ${MATH_LIBRARY_PATH_SUFFIXES}
+            )
+        if(_lib)
+            set(_libs ${_libs} ${_lib})
+        else()
+            set(${_SERVICE}_LIBRARIES ${_SERVICE}_LIBRARIES-NOTFOUND)
+            set(_libs ${_SERVICE}_LIBRARIES-NOTFOUND)
+            break()
+        endif()
+        unset (_lib CACHE)
+    endforeach()
+    set(${_SERVICE}_LIBRARIES ${_libs})
+    unset (_lib CACHE)
+    unset (_libs CACHE)
+    unset (_SERVICE)
+    unset (l)
 endmacro()
 
-macro(cache_math_result math_type _service)
-	string(TOUPPER ${_service} _SERVICE)
-	if (${_service}_h)
-		set(${_SERVICE}_H ${${_service}_h})
-	endif()
-	if (${_service}_include_dirs)
-		set(${_SERVICE}_INCLUDE_DIRS ${${_service}_include_dirs})
-	endif()
-	if (${_service}_libraries)
-		set(${_SERVICE}_LIBRARIES ${${_service}_libraries})
-	endif()
-	unset(${_service}_h)
-	unset(${_service}_include_dirs)
-	unset(${_service}_libraries)
-
+macro(cache_math_result _service MATH_TYPE)
+    string(TOUPPER ${_service} _SERVICE)
     set(${_SERVICE}_FIND_QUIETLY TRUE)
-	if (${_SERVICE}_H)
-		find_package_handle_standard_args(${_SERVICE}
-			"Could NOT find ${math_type} ${_SERVICE}"
-			${_SERVICE}_INCLUDE_DIRS ${_SERVICE}_LIBRARIES ${_SERVICE}_H)
-	else()
-		find_package_handle_standard_args(${_SERVICE}
-			"Could NOT find ${math_type} ${_SERVICE}" ${_SERVICE}_LIBRARIES)
-	endif()
+    if (DEFINED ${_SERVICE}_INCLUDE_DIRS)
+        find_package_handle_standard_args(${_SERVICE}
+            "Could NOT find ${MATH_TYPE} ${_SERVICE}"
+            ${_SERVICE}_LIBRARIES ${_SERVICE}_INCLUDE_DIRS)
+    else()
+        find_package_handle_standard_args(${_SERVICE}
+            "Could NOT find ${MATH_TYPE} ${_SERVICE}" ${_SERVICE}_LIBRARIES)
+    endif()
 
-	if (${_SERVICE}_FOUND)
-		message("-- ${math_type} ${_SERVICE} found")
-		set(HAVE_${_SERVICE} ON CACHE INTERNAL "Defined if ${_SERVICE} is available")
-		set(${_SERVICE}_LIBRARIES ${${_SERVICE}_LIBRARIES} CACHE STRING "${_SERVICE} libraries")
-		mark_as_advanced(${_SERVICE}_LIBRARIES)
-		if (${_SERVICE}_H)
-			set(${_SERVICE}_H ${${_SERVICE}_H} CACHE STRING "Name of ${_SERVICE} header")
-			set(${_SERVICE}_INCLUDE_DIRS ${${_SERVICE}_INCLUDE_DIRS}
-				CACHE STRING "${_SERVICE} include directory")
-			mark_as_advanced(${_SERVICE}_INCLUDE_DIRS ${_SERVICE}_H)
-		endif()
-	endif()
+    if (${_SERVICE}_FOUND)
+        set(${_SERVICE}_TYPE ${MATH_TYPE} CACHE STRING
+            "${_SERVICE} type")
+        mark_as_advanced(${_SERVICE}_TYPE)
+
+        add_definitions(-DHAVE_${MATH_TYPE}_${_SERVICE})
+        set(HAVE_${_SERVICE} ON CACHE INTERNAL
+            "Defined if ${_SERVICE} is available"
+            )
+        set(${_SERVICE}_LIBRARIES ${${_SERVICE}_LIBRARIES} CACHE STRING
+            "${_SERVICE} libraries"
+            )
+        mark_as_advanced(${_SERVICE}_LIBRARIES)
+        if (DEFINED ${_SERVICE}_INCLUDE_DIRS)
+            set(${_SERVICE}_H ${${_SERVICE}_H} CACHE STRING
+                "${_SERVICE} header file")
+            mark_as_advanced(${_SERVICE}_H)
+            set(${_SERVICE}_INCLUDE_DIRS ${${_SERVICE}_INCLUDE_DIRS}
+                CACHE STRING "${_SERVICE} include directory"
+                )
+            mark_as_advanced(${_SERVICE}_INCLUDE_DIRS)
+        endif()
+    else()
+        set (${_SERVICE}_LIBRARIES ${_SERVICE}_LIBRARIES-NOTFOUND)
+        if (DEFINED ${_SERVICE}_H)
+            set (${_SERVICE}_INCLUDE_DIRS ${_SERVICE}_INCLUDE_DIRS-NOTFOUND)
+            unset (${_SERVICE}_H)
+        endif()
+    endif()
+    set(${_SERVICE}_FOUND ${${_SERVICE}_FOUND} PARENT_SCOPE)
+    unset (MATH_TYPE)
+    unset (_SERVICE)
 endmacro()

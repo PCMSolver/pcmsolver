ab4b3fb16c0537bde28ba7edcdd2925e045e8603
Roberto Di Remigio <roberto.diremigio@gmail.com>
2013-05-29 17:36:09 +0200

    Added some syntactic sugar to the SurfaceFunction class. Currently not used in the user interface, some modifications there are needed.

--- src/cavity/SurfaceFunction.cpp
+++ src/cavity/SurfaceFunction.cpp
@@ -1,59 +1,109 @@
-#include <iostream>
 #include <fstream>
-#include <string>
-#include <Eigen/Dense>
+#include <stdexcept>
 
 #include "SurfaceFunction.h"
 
-using namespace std;
-using namespace Eigen;
+SurfaceFunction & SurfaceFunction::operator=(const SurfaceFunction & other)
+{
+    if (this == &other) // Check for self-assignment
+        throw std::runtime_error("Are you trying to self-assign?");
+    if (this->nPoints != other.nPoints) // Check if dimensions match
+        throw std::runtime_error("Incoherent dimensions!");
 
-
-SurfaceFunction::SurfaceFunction(const std::string & name) {
-	this->name = name;
-	allocated = false;
+    // Now swap this and other using the Copy-constructor and a non-throwing swap
+    SurfaceFunction(other).swap(*this);
+    // Old resources are released with the destruction of the temporary above
+    return *this;
 }
 
-SurfaceFunction::SurfaceFunction(const std::string & name, int nPoints) {
-	this->name = name;
-	this->values.resize(nPoints);
-	allocated = true;
+SurfaceFunction operator+(const SurfaceFunction & left, const SurfaceFunction & right)
+{
+    if (left.nPoints != right.nPoints)
+        throw std::runtime_error("Incoherent dimensions of left and right operands!");
+    std::string resultName = left.name + "+" + right.name;
+    SurfaceFunction result(resultName, left.nPoints);
+    result.values = left.values + right.values;
+    return result;
 }
 
-SurfaceFunction::SurfaceFunction(const std::string & name, int nPoints, double * values) {
-	this->name = name;
-	this->values.resize(nPoints);
-	allocated = true;
-	for (int i = 0; i < nPoints; i++) {
-		this->values(i) = values[i];
+SurfaceFunction operator-(const SurfaceFunction & left, const SurfaceFunction & right)
+{
+    if (left.nPoints != right.nPoints)
+        throw std::runtime_error("Incoherent dimensions of left and right operands!");
+    std::string resultName = left.name + "-" + right.name;
+    SurfaceFunction result(resultName, left.nPoints);
+    result.values = left.values - right.values;
+    return result;
+}
+
+SurfaceFunction & SurfaceFunction::operator+=(const SurfaceFunction & other)
+{
+    *this = *this + other;
+    return *this;
+}
+
+SurfaceFunction & SurfaceFunction::operator-=(const SurfaceFunction & other)
+{
+    *this = *this - other;
+    return *this;
+}
+
+inline void SurfaceFunction::swap(SurfaceFunction & other)
+{
+    if (this->nPoints != other.nPoints) // Check if dimensions match
+        throw std::runtime_error("Incoherent dimensions!");
+    using std::swap;
+    swap(this->name, other.name);
+    swap(this->allocate(nPoints, other.nPoints); // This is maybe redundant
+    swap(this->values, other.values);
+}
+
+inline void SurfaceFunction::swap(SurfaceFunction & left, SurfaceFunction & right)
+{
+    if (left.nPoints != right.nPoints) // Check if dimensions match
+        throw std::runtime_error("Incoherent dimensions!");
+    using std::swap;
+    swap(left.name, right.name);
+    swap(left.nPoints, right.nPoints); // This is maybe redundant
+    swap(left.values, right.values);
+}
+
+void SurfaceFunction::setValues(double * values_) 
+{
+	if (!allocated)
+		throw std::runtime_error("Surface function not allocated!");
+
+	for (int i = 0; i < nPoints; ++i) 
+	{
+		values(i) = values_[i];
 	}
 }
 
-void SurfaceFunction::setValues(double * values) {
-	for (int i = 0; i < this->values.size(); i++) {
-		this->values(i) = values[i];
+void SurfaceFunction::getValues(double * values_) 
+{
+	for (int i = 0; i < nPoints; ++i) 
+	{
+		values_[i] = values(i);
 	}
 }
 
-void SurfaceFunction::getValues(double * values) {
-	for (int i = 0; i < this->values.size(); i++) {
-		values[i] = this->values(i);
-	}
+void SurfaceFunction::clear() 
+{
+	values.setZero();
 }
 
-void SurfaceFunction::clear() {
-	this->values.setZero();
-}
-
-ostream & operator<<(ostream & os, SurfaceFunction & sf) {
+std::ostream & operator<<(std::ostream & os, SurfaceFunction & sf) 
+{
 	return sf.printObject(os);
 }
 
-ostream & SurfaceFunction::printObject(ostream & os) {
-	os << "Surface Function " << this->name << endl;
-	if (this->isAllocated()) {
-		os << values.transpose() << endl;
-	}
+std::ostream & SurfaceFunction::printObject(std::ostream & os) {
+	os << "Surface Function " << name << std::endl;
+	if (!allocated) 
+		throw std::runtime_error("Surface function not allocated!");
+	
+	os << values.transpose() << std::endl;
+	
 	return os;
 }
 
--- src/cavity/SurfaceFunction.h
+++ src/cavity/SurfaceFunction.h
@@ -8,8 +8,6 @@
 
 #include <Eigen/Dense>
 
-using namespace Eigen;
-
 /** 
 
 A basic surface function class
@@ -20,16 +18,53 @@ written by L. Frediani 2012
 class SurfaceFunction
 {
  public:
-    SurfaceFunction(const std::string & name);
-    SurfaceFunction(const std::string & name, int nPoints);
-    SurfaceFunction(const std::string & name, int nPoints, double * values);
-    ~SurfaceFunction(){}
+    SurfaceFunction(const std::string & name_) : name(name_)
+	{
+		allocated = false;
+	}
+    SurfaceFunction(const std::string & name_, int nPoints_) : name(name_), nPoints(nPoints_) 
+	{
+		values.resize(nPoints);
+		allocated = true;
+	}							       
+    SurfaceFunction(const std::string & name_, int nPoints_, double * values_) : name(name_), nPoints(nPoints_)
+	{
+		values.resize(nPoints);
+		allocated = true;
+		for (int i = 0; i < nPoints; ++i)
+		{
+			values(i) = values_[i];
+		}
+	}
+    ~SurfaceFunction()
+    {
+        allocated = false;
+    }
 
-    void setValue(int index, double value) {values(index) = value;}
-    double getValue(int index) {return values(index);}
-    VectorXd & getVector(){return values;}
-    void allocate(int nPoints){values.resize(nPoints);}
-    bool isAllocated(){return allocated;}
+    /// Copy constructor
+    SurfaceFunction(const SurfaceFunction & other) : name(other.name), nPoints(other.nPoints), values(other.values)
+    {
+        allocated = true;
+    }
+
+    /// Assignment operator
+    SurfaceFunction & operator=(const SurfaceFunction & other);
+    /// Addition operator
+    friend SurfaceFunction operator+(const SurfaceFunction & left, const SurfaceFunction & right);
+    /// Subtraction operator
+    friend SurfaceFunction operator-(const SurfaceFunction & left, const SurfaceFunction & right);
+    /// Addition-assignment operator
+    SurfaceFunction & operator+=(const SurfaceFunction & other);
+    /// Subtraction-assignment operator
+    SurfaceFunction & operator-=(const SurfaceFunction & other);
+
+    inline void swap(SurfaceFunction & left, SurfaceFunction & right);
+    inline void swap(SurfaceFunction & other);
+    void setValue(int index_, double value_) { values(index_) = value_; }
+    double getValue(int index_) {return values(index_);}
+    Eigen::VectorXd & getVector(){ return values; }
+    void allocate(int nPoints_){ values.resize(nPoints_); }
+    bool isAllocated() { return allocated; }
     void clear();
 
     void setValues(double * value);
@@ -39,9 +74,10 @@ class SurfaceFunction
 
  private:
     virtual std::ostream & printObject(std::ostream & os);
-    bool allocated;
     std::string name;
-    VectorXd values;
+    int nPoints;
+    Eigen::VectorXd values;
+    bool allocated;
 };
 
 #endif

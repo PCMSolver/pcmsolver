b29275b67e4298f59ac2196ad60113702de4ee84
Roberto Di Remigio <roberto.diremigio@gmail.com>
2013-05-31 17:10:35 +0200

    Cleaner structure and better comments in the makeCavity function in GePolCavity class.

--- src/cavity/GePolCavity.cpp
+++ src/cavity/GePolCavity.cpp
@@ -13,7 +13,6 @@
 
 using std::cout;
 using std::endl;
-using namespace Eigen;
 
 #include "GePolCavity.h"
 
@@ -51,6 +50,11 @@ void GePolCavity::makeCavity(){
 
 void GePolCavity::makeCavity(int maxts, int lwork) {
         
+	// This is a wrapper for the generatecavity_cpp_ function defined in the Fortran code PEDRA.
+	// Here we allocate the necessary arrays to be passed to PEDRA, in particular we allow
+	// for the insertion of additional spheres as in the most general formulation of the
+	// GePol algorithm.
+
 	double *xtscor  = new double[maxts];
 	double *ytscor  = new double[maxts];
 	double *ztscor  = new double[maxts];
@@ -63,16 +67,26 @@ void GePolCavity::makeCavity(int maxts, int lwork) {
 	
 	int nts;
 	
-   	VectorXd xv(nSpheres + maxAddedSpheres);
-	VectorXd yv(nSpheres + maxAddedSpheres);
-	VectorXd zv(nSpheres + maxAddedSpheres);
-	VectorXd sphereRadius(nSpheres + maxAddedSpheres);
+	// Allocate vectors of size equal to nSpheres + maxAddedSpheres where maxAddedSpheres is the 
+	// maximum number of spheres we allow the algorithm to add to our original set.
+	// If this number is exceeded, then the algorithm crashes (should look into this...)
+	// After the cavity is generated we will update ALL the class data members, both related
+	// to spheres and finite elements so that the cavity is fully formed.
+
+	Eigen::VectorXd xv(nSpheres + maxAddedSpheres);
+	Eigen::VectorXd yv(nSpheres + maxAddedSpheres);
+	Eigen::VectorXd zv(nSpheres + maxAddedSpheres);
+	Eigen::VectorXd sphereRadius_(nSpheres + maxAddedSpheres); // Not to be confused with the data member!!!
 	
-	for ( int i = 0; i < nSpheres; i++ ) {
-		xv(i) = spheres[i].getSphereCenter(0);
-		yv(i) = spheres[i].getSphereCenter(1);
-		zv(i) = spheres[i].getSphereCenter(2);
-		sphereRadius(i) = spheres[i].getSphereRadius();
+	for ( int i = 0; i < nSpheres; ++i ) 
+	{
+		for ( int j = 0; j < 3; ++j )
+		{
+			xv(i) = sphereCenter(0, i);
+			yv(i) = sphereCenter(1, i);
+			zv(i) = sphereCenter(2, i);
+		}
+		sphereRadius_(i) = sphereCenter(i);
 	}
 		
 	double *xe = xv.data();
@@ -80,71 +94,91 @@ void GePolCavity::makeCavity(int maxts, int lwork) {
 	double *ze = zv.data();
 
 	double *rin = sphereRadius.data();
- 
+
+        // Go PEDRA, Go!	
 	generatecavity_cpp_(xtscor, ytscor, ztscor, ar, xsphcor, ysphcor, zsphcor, rsph, &nts, &nSpheres, 
 						xe, ye, ze, rin, &averageArea, &probeRadius, work, &lwork);
 	
-	VectorXd rtmp(nSpheres + maxAddedSpheres);
-	VectorXd xtmp(nSpheres + maxAddedSpheres);
-	VectorXd ytmp(nSpheres + maxAddedSpheres);
-	VectorXd ztmp(nSpheres + maxAddedSpheres);
+	// We now create come Eigen temporaries to be used in the post-processing of the spheres data
+	// coming out from generatecavity_cpp_
 	
-	rtmp = sphereRadius.head(nSpheres);
+	Eigen::VectorXd rtmp(nSpheres + maxAddedSpheres);
+	Eigen::VectorXd xtmp(nSpheres + maxAddedSpheres);
+	Eigen::VectorXd ytmp(nSpheres + maxAddedSpheres);
+	Eigen::VectorXd ztmp(nSpheres + maxAddedSpheres);
+
+	// The first nSpheres elements of these temporaries will still be those of the original set of spheres	
+	rtmp = sphereRadius_.head(nSpheres);
 	xtmp = xv.head(nSpheres);
 	ytmp = yv.head(nSpheres);
 	ztmp = zv.head(nSpheres);
+	// Traverse the sphereRadius vector (starting from the nSpheres index) and count the number of 
+	// additional spheres that the algorithm created (characterized by non-zero radius)
 	addedSpheres = 0;
-	for ( int i = nSpheres; i < nSpheres + addedSpheres; i++ ) {
-		if ( sphereRadius(i) != 0) {
-			rtmp << sphereRadius(i);
-			xtmp << xv(i);
-			ytmp << yv(i);
-			ztmp << zv(i);
+	for ( int i = nSpheres; i < nSpheres + maxAddedSpheres; i++ ) 
+	{
+		if ( sphereRadius_(i) != 0) 
+		{
+			rtmp(i) = sphereRadius_(i);
+			xtmp(i) = xv(i);
+			ytmp(i) = yv(i);
+			ztmp(i) = zv(i);
 			addedSpheres += 1;
 		}
 	}
-	rtmp.resize(nSpheres + addedSpheres);
-	xtmp.resize(nSpheres + addedSpheres);
-	ytmp.resize(nSpheres + addedSpheres);
-	ztmp.resize(nSpheres + addedSpheres);
-	sphereRadius.resize(nSpheres + addedSpheres);
-	xv.resize(nSpheres + addedSpheres);
-	yv.resize(nSpheres + addedSpheres);
-	zv.resize(nSpheres + addedSpheres);
-	sphereRadius = rtmp;
-    	xv = xtmp;
-	yv = ytmp;
-	zv = ztmp;
-	
-	for ( int i = 0; i < nSpheres + addedSpheres; i++ ) {
-		Vector3d coord; 
-		coord << xv(i), yv(i), zv(i);
-		spheres[i].setSphereCenter(coord);
-		spheres[i].setSphereRadius(sphereRadius(i));
+
+	std::cout << "The PEDRA algorithm added " << addedSpheres << " new spheres to the original list." << std::endl;
+	// The "intensive" part of updating the spheres related class data members will be of course
+	// executed iff addedSpheres != 0
+	if ( addedSpheres != 0 )
+	{
+		// First of all update the nSpheres
+		nSpheres += addedSpheres;
+		// Resize sphereRadius and sphereCenter...
+		sphereRadius.resize(nSpheres);
+		sphereCenter.resize(Eigen::NoChange, nSpheres);
+		// ...clear vector<Sphere> spheres...
+		spheres.clear();
+		// ...and update their content
+		for ( int i = 0; i < nSpheres; ++i )
+		{
+			sphereRadius(i) = rtmp(i);
+			for ( int j = 0; j < 3; ++j )
+			{
+				sphereCenter(0, i) = xtmp(i);
+				sphereCenter(1, i) = ytmp(i);
+				sphereCenter(2, i) = ztmp(i);
+			}
+			Vector3d cent = sphereCenter.col(i);
+			Sphere sph(cent, sphereRadius(i));
+			spheres.push_back(sph);
+		}
 	}
     	
-    nElements = int(nts);
-    elementCenter.resize(NoChange, nElements);
-    elementSphereCenter.resize(NoChange, nElements);
-    elementNormal.resize(NoChange, nElements);
-    elementArea.resize(nElements);
-    elementRadius.resize(nElements);
-    for(int i=0; i < nElements; i++){
-		elementCenter(0,i) = xtscor[i];
-		elementCenter(1,i) = ytscor[i];
-		elementCenter(2,i) = ztscor[i];
-		elementArea(i) = ar[i];
-		elementSphereCenter(0,i) = xsphcor[i];
-		elementSphereCenter(1,i) = ysphcor[i];
-		elementSphereCenter(2,i) = zsphcor[i];
-		elementRadius(i) = rsph[i];
-    }
-
-    elementNormal = elementCenter - elementSphereCenter;
-    for(int i=0; i < nElements; i++){
-		elementNormal.col(i) /= elementNormal.col(i).norm();
-	}
-
+        nElements = int(nts);                                               
+        elementCenter.resize(Eigen::NoChange, nElements);
+        elementSphereCenter.resize(Eigen::NoChange, nElements);
+        elementNormal.resize(Eigen::NoChange, nElements);
+        elementArea.resize(nElements);
+        elementRadius.resize(nElements);
+        for( int i = 0; i < nElements; ++i )
+	{
+    		elementCenter(0,i) = xtscor[i];
+    		elementCenter(1,i) = ytscor[i];
+    		elementCenter(2,i) = ztscor[i];
+    		elementArea(i) = ar[i];
+    		elementSphereCenter(0,i) = xsphcor[i];
+    		elementSphereCenter(1,i) = ysphcor[i];
+    		elementSphereCenter(2,i) = zsphcor[i];
+    		elementRadius(i) = rsph[i];
+        }
+    
+        elementNormal = elementCenter - elementSphereCenter;
+        for( int i = 0; i < nElements; ++i)
+	{
+    		elementNormal.col(i) /= elementNormal.col(i).norm();
+    	}
+    
 	delete[] xtscor;
 	delete[] ytscor;
 	delete[] ztscor;

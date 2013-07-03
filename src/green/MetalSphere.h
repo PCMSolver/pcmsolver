5c2b40beb76e749891fc1ad849229cdba2d47723
Roberto Di Remigio <roberto.diremigio@gmail.com>
2013-07-03 23:07:59 +0200

    Restored MetalSphere, PlanarInterface and GreensFunctionSum (but work to do on all three of them...)

---
+++ src/green/MetalSphere.h
@@ -0,0 +1,50 @@
+#ifndef METALSPHERE_H
+#define METALSPHERE_H
+
+#include <complex>
+
+#include "Config.h"
+
+#include "GreensFunction.h"
+
+/*! \file MetalSphere.h
+ *  \class MetalSphere
+ *  \brief Class to describe spherical metal nanoparticles.
+ *  \author Luca Frediani
+ *  \date 2011
+ *
+ *  This class is a wrapper around the FORTRAN routines written
+ *  by Stefano Corni et al. to take into account the presence
+ *  of metal nanoparticles.
+ */
+
+
+class MetalSphere : public GreensFunction
+{
+	private:
+		typedef std::complex<double> dcomplex;
+	public:
+		MetalSphere(int how_, double eps_, double epsRe_, double epsIm_, Eigen::Vector3d & pos_, double radius_)
+			: GreensFunction(how_, false), epsSolvent(eps_), epsMetal(dcomplex(epsRe_, epsIm_)), sphPosition(pos_), sphRadius(radius_) 
+                virtual ~MetalSphere() {}                                              
+ 		virtual void compDiagonal(const Eigen::VectorXd & elementArea_, const Eigen::VectorXd & elementRadius_,
+                                          Eigen::MatrixXd & S_, Eigen::MatrixXd & D_) const;
+	        virtual double getDielectricConstant() const {}	
+	private:
+                double epsSolvent;
+                dcomplex epsMetal;
+                Eigen::Vector3d sphPosition;
+                double sphRadius;
+	private:
+		virtual	Eigen::Array4d numericalDirectional(Eigen::Vector3d & sourceNormal_, Eigen::Vector3d & source_, 
+				 			    Eigen::Vector3d & probeNormal_, Eigen::Vector3d & probe_) const;
+		virtual	Eigen::Array4d analyticDirectional(Eigen::Vector3d & sourceNormal_, Eigen::Vector3d & source_,
+				 			   Eigen::Vector3d & probeNormal_, Eigen::Vector3d & probe_) const;
+		virtual	Eigen::Array4d automaticDirectional(Eigen::Vector3d & sourceNormal_, Eigen::Vector3d & source_,
+							    Eigen::Vector3d & probeNormal_, Eigen::Vector3d & probe_) const;  
+		virtual	Eigen::Array4d automaticGradient(Eigen::Vector3d & sourceNormal_, Eigen::Vector3d & source_, 
+				                         Eigen::Vector3d & probeNormal_, Eigen::Vector3d & probe_) const;
+		virtual	Eigen::Array4d automaticHessian(Eigen::Vector3d & sourceNormal_, Eigen::Vector3d & source_, 
+				                        Eigen::Vector3d & probeNormal_, Eigen::Vector3d & probe_) const;
+};
+#endif // METALSPHERE_H

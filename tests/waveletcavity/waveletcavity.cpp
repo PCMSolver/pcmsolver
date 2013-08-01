76263946cfe04c5ef5465f56d44f5d464cf9b433
Roberto Di Remigio <roberto.diremigio@gmail.com>
2013-08-01 16:23:19 +0200

    Changed order of tests for wavelet cavity generator (NH3 test now runs before unit sphere)

--- tests/waveletcavity/waveletcavity.cpp
+++ tests/waveletcavity/waveletcavity.cpp
@@ -8,6 +8,7 @@
 #include "WaveletCavity.hpp"
 #include "gtest/gtest.h"
 
+/*
 class WaveletCavityTest : public ::testing::Test
 {
 	protected:
@@ -60,6 +61,7 @@ TEST_F(WaveletCavityTest, volume)
 	EXPECT_DOUBLE_EQ(volume, actualVolume);
 //	EXPECT_NEAR(volume, actualVolume, 1.0e-12);
 }
+*/
 
 class WaveletCavityNH3Test : public ::testing::Test
 {
@@ -119,4 +121,57 @@ TEST_F(WaveletCavityNH3Test, volume)
 	}
 	actualVolume /= 3;
 	EXPECT_NEAR(volume, actualVolume, 1.0e-12);
+}
+
+class WaveletCavityTest : public ::testing::Test
+{
+	protected:
+		WaveletCavity cav;
+		virtual void SetUp()
+		{
+			Eigen::Vector3d N(0.0, 0.0, 0.0);
+			std::vector<Sphere> spheres;
+			Sphere sph1(N, 1.0);
+			spheres.push_back(sph1);
+			double probeRadius = 1.385; // Probe Radius for water
+			cav = WaveletCavity(spheres, probeRadius);
+			cav.readCavity("molec_dyadic.dat");
+			double permittivity = 78.39;
+			Vacuum * gfInside = new Vacuum(2); // Automatic directional derivative
+			UniformDielectric * gfOutside = new UniformDielectric(2, permittivity);
+			int firstKind = 0;
+			PWCSolver solver(gfInside, gfOutside, firstKind);
+			solver.buildSystemMatrix(cav);
+			cav.uploadPoints(solver.getQuadratureLevel(), solver.getT_(), false);
+		}
+};
+
+TEST_F(WaveletCavityTest, size)
+{
+	int size = 4864;
+	int actualSize = cav.size();
+	EXPECT_EQ(size, actualSize);
+}
+
+TEST_F(WaveletCavityTest, area)
+{
+	double area = 4.0 * M_PI * pow(1.0, 2);
+ 	double actualArea = cav.getElementArea().sum();
+	EXPECT_DOUBLE_EQ(area, actualArea);
+//	EXPECT_NEAR(area, actualArea, 1.0e-12);
+}
+
+TEST_F(WaveletCavityTest, volume)
+{
+	double volume = 4.0 * M_PI * pow(1.0, 3) / 3.0;
+	Eigen::Matrix3Xd elementCenter = cav.getElementCenter();
+	Eigen::Matrix3Xd elementNormal = cav.getElementNormal();
+	double actualVolume = 0;
+        for ( int i = 0; i < cav.size(); ++i )
+	{
+		actualVolume += cav.getElementArea(i) * elementCenter.col(i).dot(elementNormal.col(i));
+	}
+	actualVolume /= 3;
+	EXPECT_DOUBLE_EQ(volume, actualVolume);
+//	EXPECT_NEAR(volume, actualVolume, 1.0e-12);
 }

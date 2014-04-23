0300dde1551c9da859817bd9d0d61c4ca1c5ec24
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-04-23 14:19:06 +0200

    Update pimpls

--- src/utils/cnpyPimpl.hpp
+++ src/utils/cnpyPimpl.hpp
@@ -8,7 +8,7 @@
 #pragma GCC diagnostic ignored "-Weffc++"
 #pragma GCC diagnostic ignored "-Wextra"
 #elif defined(__ICC) || defined(__INTEL_COMPILER)
-#pragma warning push
+#pragma warning(push, 0)
 #pragma warning disable "-Wall"
 #elif defined(__clang__)
 #pragma clang diagnostic push
@@ -24,7 +24,7 @@
 #if (defined(__GNUC__) || defined(__GNUG__)) && !(defined(__clang__) || defined(__INTEL_COMPILER))
 #pragma GCC diagnostic pop
 #elif defined(__ICC) || defined(__INTEL_COMPILER)
-#pragma warning pop
+#pragma warning(pop)
 #elif defined(__clang__)
 #pragma clang diagnostic pop 
 #endif

1d2bd08aeb9a7cac1234329869a30c01ae333461
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-04-23 09:54:21 +0200

    Rewrite pimpls

--- src/utils/GetkwPimpl.hpp
+++ src/utils/GetkwPimpl.hpp
@@ -7,13 +7,9 @@
 #pragma GCC diagnostic ignored "-Wall"
 #pragma GCC diagnostic ignored "-Weffc++"
 #pragma GCC diagnostic ignored "-Wextra"
-#include "Getkw.h"
-#pragma GCC diagnostic pop
 #elif defined(__ICC) || defined(__INTEL_COMPILER)
 #pragma warning push
 #pragma warning disable "-Wall"
-#include "Getkw.h"
-#pragma warning pop
 #elif defined(__clang__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wall"
@@ -21,8 +17,16 @@
 #pragma clang diagnostic ignored "-Wextra"
 #pragma clang diagnostic ignored "-Wdocumentation"
 #pragma clang diagnostic ignored "-Wdeprecated-register"
+#endif
+
 #include "Getkw.h"
-#pragma clang diagnostic pop
+
+#if (defined(__GNUC__) || defined(__GNUG__)) && !(defined(__clang__) || defined(__INTEL_COMPILER))
+#pragma GCC diagnostic pop
+#elif defined(__ICC) || defined(__INTEL_COMPILER)
+#pragma warning pop
+#elif defined(__clang__)
+#pragma clang diagnostic pop 
 #endif
 
 #endif // GETKWPIMPL_HPP
--- src/utils/cnpyPimpl.hpp
+++ src/utils/cnpyPimpl.hpp
@@ -2,18 +2,31 @@
 #define CNPYPIMPL_HPP
 
 // Disable obnoxious warnings from cnpy header
-#if defined (__GNUC__)
+#if (defined(__GNUC__) || defined(__GNUG__)) && !(defined(__clang__) || defined(__INTEL_COMPILER))
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wall"
 #pragma GCC diagnostic ignored "-Weffc++"
 #pragma GCC diagnostic ignored "-Wextra"
-#include "cnpy.hpp" 
-#pragma GCC diagnostic pop
-#elif (__INTEL_COMPILER)
+#elif defined(__ICC) || defined(__INTEL_COMPILER)
 #pragma warning push
 #pragma warning disable "-Wall"
+#elif defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wall"
+#pragma clang diagnostic ignored "-Weffc++"
+#pragma clang diagnostic ignored "-Wextra"
+#pragma clang diagnostic ignored "-Wdocumentation"
+#pragma clang diagnostic ignored "-Wdeprecated-register"
+#endif
+
 #include "cnpy.hpp" 
+
+#if (defined(__GNUC__) || defined(__GNUG__)) && !(defined(__clang__) || defined(__INTEL_COMPILER))
+#pragma GCC diagnostic pop
+#elif defined(__ICC) || defined(__INTEL_COMPILER)
 #pragma warning pop
+#elif defined(__clang__)
+#pragma clang diagnostic pop 
 #endif
 
 #endif // CNPYPIMPL_HPP

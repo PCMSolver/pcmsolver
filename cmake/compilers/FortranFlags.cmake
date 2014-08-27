9ce8d46b217d78d0ab7bac84ed9d5bcab07b1124
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-08-27 10:19:09 +0200

    Adjust compiler flags.
    First suggestion from this blog post: http://voices.canonical.com/jussi.pakkanen/2013/03/26/a-list-of-common-cmake-antipatterns/

--- cmake/compilers/FortranFlags.cmake
+++ cmake/compilers/FortranFlags.cmake
@@ -1,107 +1,72 @@
 if (NOT DEFINED DEFAULT_Fortran_FLAGS_SET OR RESET_FLAGS)
 
 if(CMAKE_Fortran_COMPILER_ID MATCHES GNU) # this is gfortran
-    set(CMAKE_Fortran_FLAGS         "-DVAR_GFORTRAN -DGFORTRAN=445 -fPIC -fautomatic -std=f2003")
-    set(CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g -fbacktrace -Wall -fbounds-check")
-    set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -funroll-all-loops -ftree-vectorize")
+	set(CMAKE_Fortran_FLAGS         "${CMAKE_Fortran_FLAGS} -DVAR_GFORTRAN -DGFORTRAN=445 -fPIC -fautomatic -std=f2003")
+	set(CMAKE_Fortran_FLAGS_DEBUG   "${CMAKE_Fortran_FLAGS_DEBUG} -O0 -g -fbacktrace -Wall -fbounds-check")
+	set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_RELEASE} -O3 -funroll-all-loops -ftree-vectorize")
     if(ENABLE_64BIT_INTEGERS)
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS} -fdefault-integer-8"
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fdefault-integer-8")
     endif()
     if(ENABLE_BOUNDS_CHECK)
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS} -fbounds-check"
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fbounds-check")
     endif()
     if(ENABLE_CODE_COVERAGE)
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS} -ftest-coverage"
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -ftest-coverage")
     endif()
 endif()
 
 if(CMAKE_Fortran_COMPILER_ID MATCHES G95)
-    set(CMAKE_Fortran_FLAGS         "-Wno=155 -fno-second-underscore -DVAR_G95 -fsloppy-char -fPIC")
-    set(CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g -ftrace=full")
-    set(CMAKE_Fortran_FLAGS_RELEASE "-O3")
+	set(CMAKE_Fortran_FLAGS         "${CMAKE_Fortran_FLAGS} -Wno=155 -fno-second-underscore -DVAR_G95 -fsloppy-char -fPIC")
+	set(CMAKE_Fortran_FLAGS_DEBUG   "${CMAKE_Fortran_FLAGS_DEBUG} -O0 -g -ftrace=full")
+	set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_RELEASE} -O3")
     if(ENABLE_64BIT_INTEGERS)
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS} -i8"
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -i8")
     endif()
     if(ENABLE_BOUNDS_CHECK)
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS} -Wall -fbounds-check"
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Wall -fbounds-check")
     endif()
     if(ENABLE_CODE_COVERAGE)
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS}"
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS}")
     endif()
 endif()
 
 if(CMAKE_Fortran_COMPILER_ID MATCHES Intel)
     add_definitions(-DVAR_IFORT)
-    set(CMAKE_Fortran_FLAGS         "-w -fpp -assume byterecl -traceback -fPIC -nosave")
-    set(CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g -warn all -ftrapuv")
-    set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -ip")
+    set(CMAKE_Fortran_FLAGS         "${CMAKE_Fortran_FLAGS} -w -fpp -assume byterecl -traceback -fPIC -nosave")
+    set(CMAKE_Fortran_FLAGS_DEBUG   "${CMAKE_Fortran_FLAGS_DEBUG} -O0 -g -warn all -ftrapuv")
+    set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_RELEASE} -O3 -ip")
     if(ENABLE_64BIT_INTEGERS)
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS} -i8"
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -i8")
     endif()
     if(ENABLE_BOUNDS_CHECK)
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS} -check bounds -fpstkchk -check pointers -check uninit -check output_conversion"
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -check bounds -fpstkchk -check pointers -check uninit -check output_conversion")
     endif()
 endif()
 
 if(CMAKE_Fortran_COMPILER_ID MATCHES PGI)
-    set(CMAKE_Fortran_FLAGS         "-DVAR_PGF90")
-    set(CMAKE_Fortran_FLAGS_DEBUG   "-g -O0 -Mframe")
-    set(CMAKE_Fortran_FLAGS_RELEASE "-O2 -mcmodel=medium -fast -Munroll")
+	set(CMAKE_Fortran_FLAGS         "${CMAKE_Fortran_FLAGS} -DVAR_PGF90")
+	set(CMAKE_Fortran_FLAGS_DEBUG   "${CMAKE_Fortran_FLAGS_DEBUG} -g -O0 -Mframe")
+	set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_RELEASE} -O2 -mcmodel=medium -fast -Munroll")
     if(ENABLE_64BIT_INTEGERS)
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS} -m64 -i8"
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -m64 -i8")
     else()
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS} -m32"
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -m32")
     endif()
     if(ENABLE_BOUNDS_CHECK)
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS} "
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ")
     endif()
     if(ENABLE_CODE_COVERAGE)
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS} "
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} ")
     endif()
 endif()
 
 if(CMAKE_Fortran_COMPILER_ID MATCHES XL)
-    set(CMAKE_Fortran_FLAGS         "-qzerosize -qextname")
-    set(CMAKE_Fortran_FLAGS_DEBUG   "-g")
-    set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
+	set(CMAKE_Fortran_FLAGS         "${CMAKE_Fortran_FLAGS} -qzerosize -qextname")
+	set(CMAKE_Fortran_FLAGS_DEBUG   "${CMAKE_Fortran_FLAGS_DEBUG} -g")
+	set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_RELEASE} -O2")
     if(ENABLE_64BIT_INTEGERS)
-        set(CMAKE_Fortran_FLAGS
-            "${CMAKE_Fortran_FLAGS} -q64"
-            )
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -q64")
     endif()
-
-    set_source_files_properties(${FREE_FORTRAN_SOURCES}
-        PROPERTIES COMPILE_FLAGS
-        "-qfree"
-        )
-    set_source_files_properties(${FIXED_FORTRAN_SOURCES}
-        PROPERTIES COMPILE_FLAGS
-        "-qfixed"
-        )
 endif()
 
 save_compiler_flags(Fortran)

9149f5b86c259f62f76b003e76ae9a0b88f45d48
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-08-30 19:48:19 +0200

    Making docs updates bar charts.

--- cmake/ConfigDocumentation.cmake
+++ cmake/ConfigDocumentation.cmake
@@ -1,20 +1,31 @@
 set(Doxygen_FIND_QUIETLY TRUE)
 include(FindDoxygen)
+
+macro(update_bar_charts)
+	execute_process(COMMAND "${PYTHON_EXECUTABLE}" "${PROJECT_BINARY_DIR}/bin/counter.py")
+	file(GLOB scripts "${PROJECT_SOURCE_DIR}/doc/gfx/matplotlib/*.py")
+        foreach(script ${scripts})                                                               
+        	# Update bar charts
+                execute_process(COMMAND "${PYTHON_EXECUTABLE}" "${script}" 
+                                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/doc/gfx)
+        endforeach()
+endmacro()
+
 if(DOXYGEN_FOUND)
     configure_file(
         ${CMAKE_SOURCE_DIR}/doc/Doxyfile.in
-        ${PROJECT_BINARY_DIR}/Doxyfile
-    )
+        ${PROJECT_BINARY_DIR}/Doxyfile)
+    message(STATUS "Updating bar charts...")
+    update_bar_charts()
+    message(STATUS "    ...Done!")
     add_custom_target(
         doc 
         COMMAND ${DOXYGEN_EXECUTABLE}
-        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-        )
+        WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
     add_custom_target(
 	update_gh-pages
-	COMMAND python ${PROJECT_BINARY_DIR}/bin/update_gh-pages.py
-	WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-	)
+	COMMAND "${PYTHON_EXECUTABLE}" "${PROJECT_BINARY_DIR}/bin/update_gh-pages.py"
+	WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
 else()
 	message(STATUS "Doxygen missing. You won't be able to create docs.")
 endif()

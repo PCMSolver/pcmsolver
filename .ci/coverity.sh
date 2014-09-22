9f955b5a215d7bf25e90ee043e1c351f085c9c3d
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-09-22 15:14:49 +0200

    One more iteration on Travis-Coverity.

--- .ci/coverity.sh
+++ .ci/coverity.sh
@@ -4,14 +4,14 @@
 # status code 1 whenever it (1) fails OR (2) is not on the correct
 # branch.
 set +e
-curl -o https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh travisci_build_coverity_scan.sh
+curl https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh > travisci_build_coverity_scan.sh
 sed -i -e 's@https:\/\/scan.coverity.com\/download\/@https:\/\/scan.coverity.com\/download\/cxx\/@g' travisci_build_coverity_scan.sh  
 COVERITY_SCAN_PROJECT_NAME="PCMSolver/pcmsolver" \
 COVERITY_SCAN_NOTIFICATION_EMAIL="roberto.d.remigio@uit.no" \
 COVERITY_SCAN_BRANCH_PATTERN="release" \
 COVERITY_SCAN_BUILD_COMMAND_PREPEND="python setup --cxx=g++ --cc=gcc --fc=gfortran --type=debug --tests --coverage" \
 COVERITY_SCAN_BUILD_COMMAND="make" \
-bash travisci_build_coverity_scan.sh
+sh travisci_build_coverity_scan.sh
 set -e
 
 exit 0

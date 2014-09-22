99338e3deaee37a59de331278abbee7fa737cdbb
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-09-22 15:41:40 +0200

    Travis submission script for Coverity is inside .ci directory.

--- .ci/coverity.sh
+++ .ci/coverity.sh
@@ -4,8 +4,6 @@
 # status code 1 whenever it (1) fails OR (2) is not on the correct
 # branch.
 set +e
-curl https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh > travisci_build_coverity_scan.sh
-sed -i -e 's@https:\/\/scan.coverity.com\/download\/@https:\/\/scan.coverity.com\/download\/cxx\/@g' travisci_build_coverity_scan.sh  
 COVERITY_SCAN_PROJECT_NAME="PCMSolver/pcmsolver" \
 COVERITY_SCAN_NOTIFICATION_EMAIL="roberto.d.remigio@uit.no" \
 COVERITY_SCAN_BRANCH_PATTERN="release" \

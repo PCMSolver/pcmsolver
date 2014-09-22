72a16cbe7a8357e19cd4bab7b4882d8e5278467f
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-09-22 16:16:09 +0200

    Let's get the Dropbox solution to work

--- .ci/travisci_build_coverity_scan.sh
+++ .ci/travisci_build_coverity_scan.sh
@@ -53,14 +53,14 @@ if [ ! -d $TOOL_BASE ]; then
   # Download Coverity Scan Analysis Tool
   if [ ! -e $TOOL_ARCHIVE ]; then
     echo -e "\033[33;1mDownloading Coverity Scan Analysis Tool...\033[0m"
-    wget -nv -O $TOOL_ARCHIVE $TOOL_URL --post-data "project=$COVERITY_SCAN_PROJECT_NAME&token=$COVERITY_SCAN_TOKEN"
+#    wget -nv -O $TOOL_ARCHIVE $TOOL_URL --post-data "project=$COVERITY_SCAN_PROJECT_NAME&token=$COVERITY_SCAN_TOKEN"
+    wget -nv -O $TOOL_ARCHIVE $TOOL_URL
   fi
 
   # Extract Coverity Scan Analysis Tool
   echo -e "\033[33;1mExtracting Coverity Scan Analysis Tool...\033[0m"
   mkdir -p $TOOL_BASE
   pushd $TOOL_BASE
-  file $TOOL_ARCHIVE
   tar xzf $TOOL_ARCHIVE
   popd
 fi

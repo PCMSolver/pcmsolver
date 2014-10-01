d70a360a5544cc0427f2a657a5d9c465041b8274
Monica Bugeanu (iMac) <monica.bugeanu@unibas.ch>
2014-10-01 18:45:11 +0200

    test not working

---
+++ tests/wem_lin/wem_lin_point.cpp
@@ -0,0 +1,87 @@
+/* pcmsolver_copyright_start */
+/*
+ *     PCMSolver, an API for the Polarizable Continuum Model
+ *     Copyright (C) 2013 Roberto Di Remigio, Luca Frediani and contributors
+ *     
+ *     This file is part of PCMSolver.
+ *     
+ *     PCMSolver is free software: you can redistribute it and/or modify       
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *     
+ *     PCMSolver is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *     
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with PCMSolver.  If not, see <http://www.gnu.org/licenses/>.
+ *     
+ *     For information on the complete list of contributors to the
+ *     PCMSolver API, see: <http://pcmsolver.github.io/pcmsolver-doc>
+ */
+/* pcmsolver_copyright_end */
+
+#define BOOST_TEST_MODULE PWLSolver
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+#include <iostream>
+#include <vector>
+
+#include "Config.hpp"
+
+#include <Eigen/Dense>
+
+#include "CollocationIntegrator.hpp"
+#include "DerivativeTypes.hpp"
+#include "PWLSolver.hpp"
+#include "UniformDielectric.hpp"
+#include "Vacuum.hpp"
+#include "WaveletCavity.hpp"
+
+/*! \class PWLSolver
+ *  \test \b pointCharge tests PWLSolver using a point charge with a wavelet cavity
+ */
+BOOST_AUTO_TEST_CASE(pointCharge)
+{
+    // Set up cavity
+    Eigen::Vector3d N(0.0, 0.0, 0.0);
+    std::vector<Sphere> spheres;
+    Sphere sph1(N, 2.929075493);
+    spheres.push_back(sph1);
+    double probeRadius = 1.385;
+    int patchLevel = 2;
+    double coarsity = 0.5;
+    WaveletCavity cavity(spheres, probeRadius, patchLevel, coarsity);
+    cavity.readCavity("molec_dyadic.dat");
+    // The point charge is located at the origin.
+    // The potential at cavity point s_I is Q/|s_I|
+    CollocationIntegrator * diag = new CollocationIntegrator();
+    double permittivity = 78.39;
+    Vacuum<AD_directional> * gfInside = new Vacuum<AD_directional>(diag);
+    UniformDielectric<AD_directional> * gfOutside = new
+    UniformDielectric<AD_directional>(permittivity, diag);
+    int firstKind = 0;
+    PWLSolver solver(gfInside, gfOutside, firstKind);
+    solver.buildSystemMatrix(cavity);
+    cavity.uploadPoints(solver.getQuadratureLevel(), solver.getT_(), true);
+
+    double charge = 8.0;
+    int size = cavity.size();
+    Eigen::VectorXd fake_mep = Eigen::VectorXd::Zero(size);
+    for (int i = 0; i < size; ++i) {
+        Eigen::Vector3d center = cavity.elementCenter(i);
+        double distance = center.norm();
+        fake_mep(i) = charge / distance;
+    }
+    // The total ASC for a dielectric is -Q*[(epsilon-1)/epsilon]
+    Eigen::VectorXd fake_asc = Eigen::VectorXd::Zero(size);
+    solver.compCharge(fake_mep, fake_asc);
+    double totalASC = - charge * (permittivity - 1) / permittivity;
+    double totalFakeASC = fake_asc.sum();
+    std::cout << "totalASC -totalFakeASC = " << totalASC - totalFakeASC << std::endl;
+    BOOST_REQUIRE_CLOSE(totalASC, totalFakeASC, 3e-3);
+}

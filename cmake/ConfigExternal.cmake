81f91e17562db1466df3b78ebcd28df6988fb9d1
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-10-05 18:01:00 +0200

    Tweak add_external macro

--- cmake/ConfigExternal.cmake
+++ cmake/ConfigExternal.cmake
@@ -1,14 +1,14 @@
-include(FindGit)
 
-if (GIT_FOUND)
-    add_custom_target(
-        git_update
-        COMMAND ${GIT_EXECUTABLE} submodule init
-        COMMAND ${GIT_EXECUTABLE} submodule update
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
-else()
-    if(DEVELOPMENT_CODE)
-        message("-- Git not found. You need Git for the Git submodule mechanism to work.")
+if(DEVELOPMENT_CODE)
+    include(FindGit)
+    if(GIT_FOUND)
+        add_custom_target(
+            git_update
+            COMMAND ${GIT_EXECUTABLE} submodule init
+            COMMAND ${GIT_EXECUTABLE} submodule update
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+    else()
+            message("-- Git not found. You need Git for the Git submodule mechanism to work.")
     endif()
 endif()
 
@@ -21,40 +21,62 @@ macro(add_external _project)
     else()
         set(UPDATE_COMMAND echo)
     endif()
+    
+    add_custom_target(
+        check_external_timestamp_${_project}
+        COMMAND python ${PROJECT_SOURCE_DIR}/cmake/check_external_timestamp.py
+                       ${PROJECT_BINARY_DIR}/external/${_project}-stamp/${_project}-configure
+                       ${PROJECT_BINARY_DIR}/external/${_project}-stamp
+                       ${PROJECT_SOURCE_DIR}/external/${_project}
+    )
+
+    # Set the testing command
+    set(_testing_command "${ARGV1}")
+    # Set the external project args. 
+    # In case no argument was passed for this, use the global variable
+    set(_external_project_cmake_args "")
+    if("${ARGV2}" STREQUAL "")
+	    set(_external_project_cmake_args "${ExternalProjectCMakeArgs}")
+    else()
+	    set(_external_project_cmake_args "${ARGV2}")
+    endif()
+    
+    if("${_testing_command}" STREQUAL "")
+       ExternalProject_Add(${_project}                                 
+           DOWNLOAD_COMMAND ${UPDATE_COMMAND}
+           DOWNLOAD_DIR ${PROJECT_SOURCE_DIR}
+           SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/${_project}
+           BINARY_DIR ${PROJECT_BINARY_DIR}/external/${_project}-build
+           STAMP_DIR ${PROJECT_BINARY_DIR}/external/${_project}-stamp
+           TMP_DIR ${PROJECT_BINARY_DIR}/external/${_project}-tmp
+           INSTALL_DIR ${PROJECT_BINARY_DIR}/external
+           CMAKE_ARGS ${_external_project_cmake_args}
+           )
+    else()
+       # For unfathomable reasons, CMake expects the TEST_COMMAND to be a ;-separated list...
+       separate_arguments(_testing_command)
+       ExternalProject_Add(${_project}                                 
+           DOWNLOAD_COMMAND ${UPDATE_COMMAND}
+           DOWNLOAD_DIR ${PROJECT_SOURCE_DIR}
+           SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/${_project}
+           BINARY_DIR ${PROJECT_BINARY_DIR}/external/${_project}-build
+           STAMP_DIR ${PROJECT_BINARY_DIR}/external/${_project}-stamp
+           TMP_DIR ${PROJECT_BINARY_DIR}/external/${_project}-tmp
+           INSTALL_DIR ${PROJECT_BINARY_DIR}/external
+           CMAKE_ARGS ${_external_project_cmake_args}
+	   TEST_BEFORE_INSTALL 1
+	   TEST_COMMAND "${_testing_command}"
+	   LOG_TEST 1
+           )
+    endif()	    
+
+    include_directories(${PROJECT_BINARY_DIR}/external/${_project}-build)
+    link_directories(${PROJECT_BINARY_DIR}/external/lib)
+    link_directories(${PROJECT_BINARY_DIR}/external/${_project}-build/external/lib)
 
     if(DEVELOPMENT_CODE)
-        ExternalProject_Add(${_project}                                                 
-            DOWNLOAD_COMMAND ${UPDATE_COMMAND}
-            DOWNLOAD_DIR ${PROJECT_SOURCE_DIR}
-            PREFIX ${PROJECT_SOURCE_DIR}/external
-            SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/${_project}
-            BINARY_DIR ${PROJECT_BINARY_DIR}/external/${_project}-build
-            STAMP_DIR ${PROJECT_BINARY_DIR}/external/${_project}-stamp
-            TMP_DIR ${PROJECT_BINARY_DIR}/external/${_project}-tmp
-            INSTALL_DIR ${PROJECT_BINARY_DIR}/external
-            CMAKE_ARGS ${ExternalProjectCMakeArgs})
-        include_directories(${PROJECT_BINARY_DIR}/external/${_project}-build)
-        link_directories(${PROJECT_BINARY_DIR}/external/lib)
-        link_directories(${PROJECT_BINARY_DIR}/external/${_project}-build/external/lib)
         add_dependencies(${_project} git_update)
-    else()
-        ExternalProject_Add(${_project}                                                 
-            PREFIX ${PROJECT_SOURCE_DIR}/external
-            SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/${_project}
-            BINARY_DIR ${PROJECT_BINARY_DIR}/external/${_project}-build
-            STAMP_DIR ${PROJECT_BINARY_DIR}/external/${_project}-stamp
-            TMP_DIR ${PROJECT_BINARY_DIR}/external/${_project}-tmp
-            INSTALL_DIR ${PROJECT_BINARY_DIR}/external
-            CMAKE_ARGS ${ExternalProjectCMakeArgs})
-        include_directories(${PROJECT_BINARY_DIR}/external/${_project}-build)
-        link_directories(${PROJECT_BINARY_DIR}/external/lib)
-        link_directories(${PROJECT_BINARY_DIR}/external/${_project}-build/external/lib)
+        add_dependencies(check_external_timestamp_${_project} git_update)
     endif()
-
-   # remove stamps for external builds so that they are rebuilt every time
-   add_custom_command(
-       TARGET ${_project}
-       PRE_BUILD
-       COMMAND rm -rf ${PROJECT_BINARY_DIR}/external/${_project}-stamp
-       WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+    add_dependencies(${_project} check_external_timestamp_${_project})
 endmacro()

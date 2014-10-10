79d803f638239459f9acd2eee7103fdbb549ce42
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-10-10 16:15:18 +0200

    Indentation with Artistic Style

--- src/utils/GreenData.hpp
+++ src/utils/GreenData.hpp
@@ -2,22 +2,22 @@
 /*
  *     PCMSolver, an API for the Polarizable Continuum Model
  *     Copyright (C) 2013 Roberto Di Remigio, Luca Frediani and contributors
- *     
+ *
  *     This file is part of PCMSolver.
- *     
- *     PCMSolver is free software: you can redistribute it and/or modify       
+ *
+ *     PCMSolver is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Lesser General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
- *     
+ *
  *     PCMSolver is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU Lesser General Public License for more details.
- *     
+ *
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with PCMSolver.  If not, see <http://www.gnu.org/licenses/>.
- *     
+ *
  *     For information on the complete list of contributors to the
  *     PCMSolver API, see: <http://pcmsolver.github.io/pcmsolver-doc>
  */
@@ -62,7 +62,8 @@ struct greenData {
     double sphereRadius;
     DiagonalIntegrator * integrator;
 
-    greenData(int _how, double _epsilon = 1.0, DiagonalIntegrator * _integrator = NULL, double _kappa = 0.0,
+    greenData(int _how, double _epsilon = 1.0, DiagonalIntegrator * _integrator = NULL,
+              double _kappa = 0.0,
               double _epsReal = 0.0, double _epsImaginary = 0.0,
               const std::vector<double> & _sphere = std::vector<double>(),
               double _sphRadius = 0.0) :

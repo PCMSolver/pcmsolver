9c70370e0a903cf196a5557ee1d61803e71cd47b
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-10-10 16:32:32 +0200

    Solve mess with libgetkw. It had been accidentally removed in a previous commit...

---
+++ external/libgetkw/Fortran/kinds.f90
@@ -0,0 +1,10 @@
+module kinds_m
+	implicit none
+	integer, parameter :: MP = kind(0)
+	integer, parameter :: SP = kind(0.0e0)
+	integer, parameter :: DP = kind(0.0d0)
+
+	integer, parameter :: I2 = 2
+	integer, parameter :: I4 = 4
+	integer, parameter :: I8 = 8
+end module 

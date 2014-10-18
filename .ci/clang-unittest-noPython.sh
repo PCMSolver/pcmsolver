55026a1805a5395fd485f035c9e9e171f7574874
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-10-18 16:52:14 +0200

    Add another build (without Python embedding) to .travis.yml

--- .ci/clang-unittest-noPython.sh
+++ .ci/clang-unittest-noPython.sh
@@ -1,6 +1,6 @@
 . "$CI_SCRIPTS/common.sh"
 
-python setup --cxx=clang++ --cc=clang --fc=gfortran --type=release --tests
+python setup --cxx=clang++ --cc=clang --fc=gfortran --type=release --tests --disable_python_embedding
 cd build
 make
 make test

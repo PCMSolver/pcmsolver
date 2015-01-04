9ee569a6156ed5fd51dcc62ce7f1bf1991a305d0
Roberto Di Remigio <roberto.d.remigio@uit.no>
2015-01-04 19:04:43 +0100

    Compiles. Tests pass

--- src/utils/Input.hpp
+++ src/utils/Input.hpp
@@ -50,8 +50,6 @@
  *  They must be specified as private data members with public accessor methods (get-ters).
  *  In general, no mutator methods (set-ters) should be needed, exceptions to this rule
  *  should be carefully considered.
- *  \warning If new data members are added, you **must** update the definition of the copy
- *  constructor and of the two swap functions.
  */
 
 class Input
@@ -63,15 +61,7 @@ public:
     Input(const std::string & filename);
     /// Constructor from host input structs
     Input(const cavityInput & cav, const solverInput & solv, const greenInput & green);
-    /// Copy constructor
-    Input(const Input &other);
-    /// Assignment operator
-    Input& operator=(Input other);
-    /// Destructor
-    ~Input();
 
-    friend inline void swap(Input & left, Input & right);
-    inline void swap(Input & other);
     // Accessor methods
     std::string units() { return units_; }
     int CODATAyear() { return CODATAyear_; }
@@ -120,9 +110,6 @@ public:
     friend std::ostream & operator<<(std::ostream &os, const Input &input);
     /// @}
 private:
-    /*! Parse input by embedding the Python pcmsolver.py script as a module.
-     */
-    void parser(const std::string & filename);
     /*! Read Python-parsed input (API-side syntactic input parsing) into Input object
      */
     void reader(const char * pythonParsed);
@@ -215,8 +202,6 @@ private:
     double sphereRadius_;
     /// Who performed the syntactic input parsing
     std::string providedBy_;
-    /// Whether a Python interpreter was already embedded
-    bool hadInterpreter_;
 };
 
 /*!

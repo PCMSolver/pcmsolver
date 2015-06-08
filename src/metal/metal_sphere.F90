ef8328a86c0f28b32c0e93062439b5671431579c
Roberto Di Remigio <roberto.d.remigio@uit.no>
2015-06-08 11:19:29 +0200

    Remove whitespaces from metal_sphere.F90

--- src/metal/metal_sphere.F90
+++ src/metal/metal_sphere.F90
@@ -1,22 +1,22 @@
 !pcmsolver_copyright_start
 !       PCMSolver, an API for the Polarizable Continuum Model
 !       Copyright (C) 2013 Roberto Di Remigio, Luca Frediani and contributors
-!       
+!
 !       This file is part of PCMSolver.
-! 
-!       PCMSolver is free software: you can redistribute it and/or modify       
+!
+!       PCMSolver is free software: you can redistribute it and/or modify
 !       it under the terms of the GNU Lesser General Public License as published by
 !       the Free Software Foundation, either version 3 of the License, or
 !       (at your option) any later version.
-!                                                                            
+!
 !       PCMSolver is distributed in the hope that it will be useful,
 !       but WITHOUT ANY WARRANTY; without even the implied warranty of
 !       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 !       GNU Lesser General Public License for more details.
-!                                                                            
+!
 !       You should have received a copy of the GNU Lesser General Public License
 !       along with PCMSolver.  If not, see <http://www.gnu.org/licenses/>.
-! 
+!
 !       For information on the complete list of contributors to the
 !       PCMSolver API, see: <http://pcmsolver.github.io/pcmsolver-doc>
 !pcmsolver_copyright_end
@@ -27,7 +27,7 @@ use, intrinsic :: iso_c_binding
 
 implicit none
 
-public greens_function 
+public greens_function
 
 private
 ! integer(kind=regint_k) types
@@ -43,36 +43,36 @@ integer, parameter :: sp = kind(1.0)
 integer, parameter :: dp = selected_real_kind(2*precision(1.0_sp))
 
 contains
-      
+
     subroutine greens_function(epssol, epsre, epsim, radius,   &
          ps, p1, p2, greenre, greenim) bind(c, name='greens_function')
 
-    ! Passed variables      
+    ! Passed variables
     real(c_double), intent(in)  :: epssol, epsre, epsim, radius
     real(c_double), intent(in)  :: p1(*), p2(*), ps(*)
     real(c_double), intent(out) :: greenre, greenim
-    ! Local variables      
+    ! Local variables
     complex(kind=dp) :: green, eps2, ui
-      
-    ui = (0.0_dp, 1.0_dp)                                               
-    eps2 = epsre + epsim * ui                                     
-    green = gsfera(epssol, eps2, ps(1), ps(2), ps(3), radius,  &  
-                  p1(1), p1(2), p1(3),                         &  
-                  p2(1), p2(2), p2(3))                            
+
+    ui = (0.0_dp, 1.0_dp)
+    eps2 = epsre + epsim * ui
+    green = gsfera(epssol, eps2, ps(1), ps(2), ps(3), radius,  &
+                  p1(1), p1(2), p1(3),                         &
+                  p2(1), p2(2), p2(3))
     greenre = real(real(green))
     greenim = real(aimag(green))
 
-    end subroutine greens_function                                
+    end subroutine greens_function
 
     complex(kind=dp) function gsfera(eps, eps2, xs, ys, zs, rs,     &
                                 xi, yi, zi, xj, yj, zj)
-    ! Passed variables 
+    ! Passed variables
     real(kind=dp), intent(in)     :: eps
     complex(kind=dp), intent(in)  :: eps2
-    real(kind=dp), intent(in)     :: xs, ys, zs, rs ! Sphere center and radius 
+    real(kind=dp), intent(in)     :: xs, ys, zs, rs ! Sphere center and radius
     real(kind=dp), intent(in)     :: xi, yi, zi     ! Source point
     real(kind=dp), intent(in)     :: xj, yj, zj     ! Probe point
-    ! Local variables          
+    ! Local variables
     complex(kind=dp)  :: coefl
     real(kind=dp)     :: di, dj
     real(kind=dp)     :: dim, xim, yim, zim, qim    ! Image quantities
@@ -86,9 +86,9 @@ contains
     di = sqrt((xi - xs)**2 + (yi - ys)**2 + (zi - zs)**2)
     dj = sqrt((xj - xs)**2 + (yj - ys)**2 + (zj - zs)**2)
     dim = rs**2 / di
-    xim = dim * (xi - xs) / (di + xs) 
-    yim = dim * (yi - ys) / (di + ys) 
-    zim = dim * (zi - zs) / (di + zs) 
+    xim = dim * (xi - xs) / (di + xs)
+    yim = dim * (yi - ys) / (di + ys)
+    zim = dim * (zi - zs) / (di + zs)
     qim = rs / di
     gc = qim / sqrt((xj - xim)**2 + (yj - yim)**2 + (zj - zim)**2)
     gc = gc - qim / sqrt((xj - xs)**2 + (yj - ys)**2 + (zj - zs)**2)
@@ -111,7 +111,7 @@ contains
     gsfera = gsfera + gc * (eps2- eps) / (eps2 + eps)
     !lf: WARNING: sign change and divided by epsilon of solvent!!!!!!
     gsfera = -gsfera / eps
-    ! DEBUGGING STUFF      
+    ! DEBUGGING STUFF
     !   arg = rs * rs / (di * dj)
     !   argl = rs / (di * dj)
     !   gsfera = 0.0d0
@@ -120,16 +120,16 @@ contains
     !     argl = argl * arg
     !     coefl = (eps2 - eps) * float(l) / ((eps2 + eps) * float(l) + 1.0d0)
     !     gsfera = gsfera + coefl * argl * legendre_polynomial(l, m, cost)
-    !   enddo 
+    !   enddo
     !lf write (6, *) "gsfera2", gsfera
     end function gsfera
-    
+
     real(kind=dp) function legendre_polynomial(l, m, x)
     ! Computes the associated Legendre polynomial P_l^m(x)
-    ! Passed variables      
+    ! Passed variables
     integer(kind=regint_k), intent(in) :: l, m
     real(kind=dp), intent(in) :: x
-    ! Local variables      
+    ! Local variables
     integer(kind=regint_k) :: i, ll
     real(kind=dp) :: fact, pll, pmm, pmmp1, somx2
 
@@ -160,4 +160,4 @@ contains
 
     end function legendre_polynomial
 
-end module metal_sphere 
+end module metal_sphere

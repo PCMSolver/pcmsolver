0f3df4c6869a4f13f21bf9a77538c8102ad40635
Roberto Di Remigio <roberto.diremigio@gmail.com>
2015-06-18 20:28:46 +0200

    Define a splineInterpolation function (to be used instead of linearInterpolation)

--- src/utils/SplineFunction.hpp
+++ src/utils/SplineFunction.hpp
@@ -40,17 +40,7 @@
  *  Taken from StackOverflow http://stackoverflow.com/a/29825204/2528668
  */
    
-/*! Scale abscissa value to [0, 1] */
-inline double scale(double x) {
-	return (x - xMin_) / (xMax_ - xMin_);
-}
-
-/*! Scale abscissa values to [0, 1] */
-inline Eigen::RowVectorXd scale(const Eigen::VectorXd & x_vec) {
-    return x_vec.unaryExpr([](double x) -> double { return scale(x); }).transpose();
-}
-
-class SplineFunction
+class SplineFunction final
 {
 private:
     typedef Eigen::Spline<double, 1, 3> SplineFit;
@@ -61,18 +51,36 @@ public:
     SplineFunction(const Eigen::VectorXd & x, const Eigen::VectorXd & y) 
 	    : xMin_(x.minCoeff()), xMax_(x.maxCoeff()),
 	    spline_(Eigen::SplineFitting<SplineFit>::Interpolate(y.transpose(), 
-		      std::min<int>(x.rows() - 1, 3), scale(x))
+		      std::min<int>(x.rows() - 1, 3), fitVector(x))
 	           )
 	{}
     /*! \brief Evaluate spline at given point
      *  \param[in] x evaluation point
      */
     double operator()(double x) const {
-	return spline_(scale(x))(0);
+	return spline_(fitScalar(x))(0);
     }
 private:
     double xMin_;
     double xMax_;
     SplineFit spline_;
+    /*! \brief Scale a scalar value to [0, 1] interval 
+     *  \param[in] x value to be scaled
+     *
+     *  val is defined in [min, max] This function returns the variable
+     *  as scaled to fit in the [0, 1] interval.
+     */
+    double fitScalar(double x) const {
+	    return (x - xMin_) / (xMax_ - xMin_);
+    }
+    /*! \brief Scale a vector to [0, 1] interval 
+     *  \param[in] vec a vector
+     *
+     *  Given column vector, returns a *row* vector with the values scaled
+     *  to fit a preset interval. The interval is embedded in the callable object.
+     */
+    Eigen::RowVectorXd fitVector(const Eigen::VectorXd & x_vec) const {
+	    return x_vec.unaryExpr([this](double x) -> double { return fitScalar(x); }).transpose();
+    }
 };
 #endif // SPLINEFUNCTION_HPP

1cd88b933064af6897234f4a9c040ea5b9b00822
Roberto Di Remigio <roberto.d.remigio@uit.no>
2015-09-23 15:40:09 +0200

    Removed unused scripts and simplify autogenerated.cmake

--- cmake/custom/autogenerated.cmake
+++ cmake/custom/autogenerated.cmake
@@ -3,43 +3,16 @@ configure_file(${PROJECT_SOURCE_DIR}/Config.hpp.in ${PROJECT_BINARY_DIR}/include
 
 get_property(PCMSOLVER_EXECUTABLE GLOBAL PROPERTY PCMSolver_EXECUTABLE)
 # Configure the input parsing script
-configure_file(${PROJECT_SOURCE_DIR}/tools/pcmsolver.py.in pcmsolver.py @ONLY)
-file(COPY ${PROJECT_BINARY_DIR}/pcmsolver.py
-    DESTINATION ${PROJECT_BINARY_DIR}/bin
-    FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ
-    GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
-
-# Configure the touch_cmakelists utility script
-configure_file(${PROJECT_SOURCE_DIR}/tools/touch_cmakelists.py.in touch_cmakelists.py @ONLY)
-file(COPY ${PROJECT_BINARY_DIR}/touch_cmakelists.py
-    DESTINATION ${PROJECT_BINARY_DIR}/bin
-    FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ
-    GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+configure_file(${PROJECT_SOURCE_DIR}/tools/pcmsolver.py.in ${PROJECT_BINARY_DIR}/bin/pcmsolver.py @ONLY)
 
 # Configure the update_gh-pages utility script
-configure_file(${PROJECT_SOURCE_DIR}/tools/update_gh-pages.py.in update_gh-pages.py @ONLY)
-file(COPY ${PROJECT_BINARY_DIR}/update_gh-pages.py
-    DESTINATION ${PROJECT_BINARY_DIR}/bin
-    FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ
-    GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+configure_file(${PROJECT_SOURCE_DIR}/tools/update_gh-pages.py.in ${PROJECT_BINARY_DIR}/bin/update_gh-pages.py @ONLY)
 
 # Configure the counter utility script
-configure_file(${PROJECT_SOURCE_DIR}/tools/counter.py.in counter.py @ONLY)
-file(COPY ${PROJECT_BINARY_DIR}/counter.py
-    DESTINATION ${PROJECT_BINARY_DIR}/bin
-    FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ
-    GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+configure_file(${PROJECT_SOURCE_DIR}/tools/counter.py.in ${PROJECT_BINARY_DIR}/bin/counter.py @ONLY)
 
 # Configure the extract_notice utility script
-configure_file(${PROJECT_SOURCE_DIR}/tools/extract_notice.py.in extract_notice.py @ONLY)
-file(COPY ${PROJECT_BINARY_DIR}/extract_notice.py
-    DESTINATION ${PROJECT_BINARY_DIR}/bin
-    FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ
-    GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+configure_file(${PROJECT_SOURCE_DIR}/tools/extract_notice.py.in ${PROJECT_BINARY_DIR}/bin/extract_notice.py @ONLY)
 
 # Configure the update_copyright utility script
-configure_file(${PROJECT_SOURCE_DIR}/tools/update_copyright.py.in update_copyright.py @ONLY)
-file(COPY ${PROJECT_BINARY_DIR}/update_copyright.py
-    DESTINATION ${PROJECT_BINARY_DIR}/bin
-    FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ
-    GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+configure_file(${PROJECT_SOURCE_DIR}/tools/update_copyright.py.in ${PROJECT_BINARY_DIR}/bin/update_copyright.py @ONLY)
--- tools/mysed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for file in `ls *.c *.h` ;
-do
-	echo $file
-	sed s/"$1"/"$2"/ $file > abc
-	cp abc $file
-done
-rm abc

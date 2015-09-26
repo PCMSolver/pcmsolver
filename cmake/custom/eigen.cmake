c92cd5d0fd310c053254d929af6942f82302170f
Roberto Di Remigio <roberto.d.remigio@uit.no>
2015-09-26 19:39:06 +0200

    Update building docs

--- cmake/custom/eigen.cmake
+++ cmake/custom/eigen.cmake
@@ -4,10 +4,8 @@ install(DIRECTORY ${PROJECT_SOURCE_DIR}/external/eigen3 DESTINATION ${PROJECT_BI
 message(STATUS "Eigen 3.2.0 is located here: " ${PROJECT_BINARY_DIR}/external/include/eigen3)
 include_directories(SYSTEM ${EIGEN3_INCLUDE_DIR})
 
-# FIXME
-#if(ENABLE_EIGEN_MKL)
-#   message(STATUS "ENABLE_EIGEN_MKL option requires Intel MKL 10.3")
-#   message(STATUS "   Be sure you have read http://eigen.tuxfamily.org/dox/TopicUsingIntelMKL.html")
-#   include(ConfigMath)
-#   set(EIGEN_USE_MKL_ALL ON)
-#endif()
+if(ENABLE_EIGEN_MKL)
+   message(STATUS "ENABLE_EIGEN_MKL option requires Intel MKL 10.3")
+   message(STATUS "   Be sure you have read http://eigen.tuxfamily.org/dox/TopicUsingIntelMKL.html")
+   set(EIGEN_USE_MKL_ALL ON)
+endif()

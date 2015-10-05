7e7549648c7a26579883e0f9ef86edce6c46cc88
Roberto Di Remigio <roberto.d.remigio@uit.no>
2015-10-05 11:17:46 +0200

    Remove sources and tests not belonging on master

--- src/wemFiles/Volume.hpp
+++
@@ -1,3 +0,0 @@
-class GenericAnsatzFunction;
-
-double calculateVolume(GenericAnsatzFunction *af);
--- src/wemSolvers/WEMPCG.hpp
+++
@@ -1,9 +0,0 @@
-/*
- * @file WEMPCG.hpp
- *
- * @brief the CG solver
- */
-#ifndef CG_HPP
-#define CG_HPP
-unsigned int WEMPCG(SparseMatrix* A, double* b, double* x, double epsi, GenericAnsatzFunction* af);
-#endif

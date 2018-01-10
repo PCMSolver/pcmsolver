f00913f0d16e1acbeb7a3eafafae73ae9ba4eeb5
Roberto Di Remigio <robertodr@users.noreply.github.com>
2018-01-10 13:12:33 +0100

    Fix Python 3 issue writing out uppercased input

--- default.nix
+++ default.nix
@@ -4,10 +4,27 @@ let
     owner = "NixOS";
     repo = "nixpkgs-channels";
     rev = "nixos-unstable";
-    sha256 = "1d4q92jw42d51s9bn380jayy2rs1v0h1y8kvrbjg3i43f72ck5q5";
+    sha256 = "0q47hjl01wylp6cdb52fyjbbx45djnz7bwfyj2mfv2lh04iph3s8";
   });
 in
-  with import nixpkgs {};
+  with import nixpkgs {
+    overlays = [(self: super:
+      # Within the overlay we use a recursive set, though I think we can use `self` as well.
+      rec {
+        # nix-shell -p python.pkgs.my_stuff
+        python3 = super.python3.override {
+          # Careful, we're using a different self and super here!
+          packageOverrides = py-self: py-super: {
+            matplotlib = py-super.matplotlib.override {
+              enableTk = true;
+              enableQt = true;
+            };
+          };
+        };
+        python3Packages = python3.pkgs;
+      }
+    )];
+  };
   stdenv.mkDerivation {
     name = "PCMSolver-dev";
     buildInputs = [
@@ -20,11 +37,14 @@ in
       cmake
       doxygen
       exa
-      #gcc
+      ffmpeg
       gfortran
       graphviz
       lldb
+      pipenv
+      python3Full
       python3Packages.docopt
+      python3Packages.jupyter
       python3Packages.matplotlib
       python3Packages.numpy
       python3Packages.pyyaml
@@ -32,4 +52,9 @@ in
       valgrind
       zlib
     ];
+    src = null;
+    shellHook = ''
+      NINJA_STATUS="[Built edge %f of %t in %e sec]"
+      SOURCE_DATE_EPOCH=$(date +%s)
+    '';
   }

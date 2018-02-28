293062d79b92af4cf0a461f56eda80e2605bad42
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-02-28 13:17:53 +0100

    Make versioning regex robust, fixes CI failures
    The current solutions is hacky, since we want to keep using pure CMake
    to extract this information. It should all be transferred to Python
    and use Git to describe the number of commits that occurred since
    a tag (as done in Psi4).

--- cmake/custom/pcmsolver.cmake
+++ cmake/custom/pcmsolver.cmake
@@ -20,7 +20,7 @@ set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_BINDIR})
 
 if(NOT DEFINED PYMOD_INSTALL_LIBDIR)
-  message(STATUS "Setting (unspecified) option ${variable}: ${default}")
+  message(STATUS "Setting (unspecified) option PYMOD_INSTALL_LIBDIR: python")
   set(PYMOD_INSTALL_LIBDIR "python" CACHE STRING "Location within CMAKE_INSTALL_LIBDIR to which Python modules are installed" FORCE)
 else()
   message(STATUS "Setting option PYMOD_INSTALL_LIBDIR: ${PYMOD_INSTALL_LIBDIR}")
--- cmake/custom/version.cmake
+++ cmake/custom/version.cmake
@@ -3,10 +3,12 @@ string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)(-[a-z,A-Z,0-9]+)?" PCMSolver_
 string(REPLACE "." ";" _version_list ${PCMSolver_VERSION})
 list(GET _version_list 0 PROJECT_VERSION_MAJOR)
 list(GET _version_list 1 PROJECT_VERSION_MINOR)
-list(GET _version_list 2 _patch)
+list(GET _version_list 2 PROJECT_VERSION_PATCH)
 # Separate on the dash
-string(REPLACE "-" ";" _repatch ${_patch})
-list(GET _repatch 0 PROJECT_VERSION_PATCH)
-list(GET _repatch 1 PROJECT_VERSION_DESCRIBE)
+string(FIND ${PROJECT_VERSION_PATCH} "-" _has_describe)
+if(NOT _has_describe EQUAL -1)
+  string(REPLACE "-" ";" _repatch ${PROJECT_VERSION_PATCH})
+  list(GET _repatch 1 PROJECT_VERSION_DESCRIBE)
+endif()
 
 message(STATUS "${BoldGreen}PCMSolver v${PCMSolver_VERSION}${ColourReset}")

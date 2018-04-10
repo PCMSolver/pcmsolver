d5ef738bdba12f8d529509d352eef08c4e647c02
Roberto Di Remigio <robertodr@users.noreply.github.com>
2018-04-10 10:00:34 -0400

    FIx handling of pcmsolver.f90 file (#143)
    * Install pcmsolver.f90 rather than compiling it
    
    * Fix documentation of configuration option

--- cmake/custom/api.cmake
+++ cmake/custom/api.cmake
@@ -1,18 +1,18 @@
 #.rst:
 #
 # Manage compilation of API.
-# Optionally, enable compilation of Fortran 90 API bindings.
+# Optionally, enable testing of Fortran 90 API bindings.
 #
 # Variables defined::
 #
-#   ENABLE_Fortran_API
+#   TEST_Fortran_API
 #
 # autocmake.yml configuration::
 #
-#   docopt: "--fbindings=<ENABLE_Fortran_API> Enable compilation of Fortran 90 API bindings <ON/OFF> [default: ON]."
-#   define: "'-DENABLE_Fortran_API={0}'.format(arguments['--fbindings'])"
+#   docopt: "--fbindings=<TEST_Fortran_API> Enable testing of Fortran 90 API bindings <ON/OFF> [default: ON]."
+#   define: "'-DTEST_Fortran_API={0}'.format(arguments['--fbindings'])"
 
-option(ENABLE_Fortran_API "Enable compilation of Fortran 90 API bindings" ON)
+option(TEST_Fortran_API "Enable testing of Fortran 90 API bindings" ON)
 
 add_subdirectory(api)
 include_directories(${PROJECT_SOURCE_DIR}/api)
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ Options:
   --int64                                Enable 64bit integers [default: False].
   --omp                                  Enable OpenMP parallelization [default: False].
   --python=<PYTHON_INTERPRETER>          The Python interpreter (development version) to use. [default: ''].
-  --fbindings=<ENABLE_Fortran_API>       Enable compilation of Fortran 90 API bindings <ON/OFF> [default: ON].
+  --fbindings=<TEST_Fortran_API>         Enable testing of Fortran 90 API bindings <ON/OFF> [default: ON].
   --boost-headers=<BOOST_INCLUDEDIR>     Include directories for Boost [default: ''].
   --boost-libraries=<BOOST_LIBRARYDIR>   Library directories for Boost [default: ''].
   --build-boost=<FORCE_CUSTOM_BOOST>     Deactivate Boost detection and build on-the-fly <ON/OFF> [default: OFF].
@@ -66,7 +66,7 @@ def gen_cmake_command(options, arguments):
     command.append('-DENABLE_64BIT_INTEGERS={0}'.format(arguments['--int64']))
     command.append('-DENABLE_OPENMP={0}'.format(arguments['--omp']))
     command.append('-DPYTHON_INTERPRETER="{0}"'.format(arguments['--python']))
-    command.append('-DENABLE_Fortran_API={0}'.format(arguments['--fbindings']))
+    command.append('-DTEST_Fortran_API={0}'.format(arguments['--fbindings']))
     command.append('-DBOOST_INCLUDEDIR="{0}"'.format(arguments['--boost-headers']))
     command.append('-DBOOST_LIBRARYDIR="{0}"'.format(arguments['--boost-libraries']))
     command.append('-DFORCE_CUSTOM_BOOST={0}'.format(arguments['--build-boost']))

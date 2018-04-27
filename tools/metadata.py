25ba4fc9a78715a44b1f9d59d58fc9f41f2bc6df
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-04-27 15:52:43 -0400

    Records tag for v1.2.0

--- tools/metadata.py
+++ tools/metadata.py
@@ -1,7 +1,7 @@
 __version__ = '1.2.0'
-__version_long = '1.2.0+zzzzzzz'
+__version_long = '1.2.0+e1943b1'
 __version_upcoming_annotated_v_tag = '1.3.0'
-__version_most_recent_release = '1.2.0'
+__version_most_recent_release = '1.2.0-rc1'
 
 
 def version_formatter(dummy):

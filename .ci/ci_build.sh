b69b31a94a4767a2bf7d7830dafb080b540c605a
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-02-07 18:28:50 +0100

    Run build within pipenv

--- .ci/ci_build.sh
+++ .ci/ci_build.sh
@@ -2,7 +2,7 @@
 
 # The return code will capture an error from ANY of the functions in the pipe
 set -euo pipefail
-cmake --build . --target install -- --jobs=2 VERBOSE=1 | tee build.log | grep "Building\|Installing"
+pipenv run cmake --build . --target install -- --jobs=2 VERBOSE=1 | tee build.log | grep "Building\|Installing"
 RESULT=$?
 
 if [ $RESULT -eq 0 ]; then

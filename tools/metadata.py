ceeca60698ab2e4f99fe38e85a9774f95f15d94b
Roberto Di Remigio <roberto.diremigio@gmail.com>
2019-02-14 09:56:24 -0500

    Bump version to v1.2.3

--- tools/metadata.py
+++ tools/metadata.py
@@ -1,7 +1,7 @@
-__version__ = '1.2.2'
-__version_long = '1.2.2+6a43953'
-__version_upcoming_annotated_v_tag = '1.2.3'
-__version_most_recent_release = '1.2.1'
+__version__ = '1.2.3'
+__version_long = '1.2.3+zzzzzzz'
+__version_upcoming_annotated_v_tag = '1.2.4'
+__version_most_recent_release = '1.2.2'
 
 
 def version_formatter(dummy):

548a88747a8a9bf5b7cb929c5aedd7f828e08aff
Lori A. Burns <lori.burns@gmail.com>
2023-04-06 15:59:53 -0400

    help boost cleanup win multirun?

--- cmake/downloaded/boost_cleanup.cmake
+++ cmake/downloaded/boost_cleanup.cmake
@@ -4,7 +4,7 @@
 # Clean-up
 add_custom_command(
     OUTPUT ${CUSTOM_BOOST_LOCATION}/boost.cleanedup
-    COMMAND ${CMAKE_COMMAND} -E remove_directory ${BOOST_BUILD_DIR}
+    COMMAND ${CMAKE_COMMAND} -E rm -r ${BOOST_BUILD_DIR}
     COMMAND ${CMAKE_COMMAND} -E touch ${CUSTOM_BOOST_LOCATION}/boost.cleanedup
     WORKING_DIRECTORY ${CUSTOM_BOOST_LOCATION}
     DEPENDS ${CUSTOM_BOOST_LOCATION}/boost.installed

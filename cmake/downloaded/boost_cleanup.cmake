8f72076491acc9b45b666c3adc5c605e0d666b12
Lori A. Burns <lori.burns@gmail.com>
2023-04-07 15:08:31 -0400

    still need tgt

--- cmake/downloaded/boost_cleanup.cmake
+++ cmake/downloaded/boost_cleanup.cmake
@@ -2,8 +2,15 @@
 # licensed under BSD-3: https://github.com/coderefinery/autocmake/blob/master/LICENSE
 
 # Clean-up
-if(NOT WIN32)
+if(WIN32)
   # observed trouble deleting the directory on Windows + MinGW + multiple builds
+  add_custom_command(
+    OUTPUT ${CUSTOM_BOOST_LOCATION}/boost.cleanedup
+    COMMAND ${CMAKE_COMMAND} -E touch ${CUSTOM_BOOST_LOCATION}/boost.cleanedup
+    WORKING_DIRECTORY ${CUSTOM_BOOST_LOCATION}
+    DEPENDS ${CUSTOM_BOOST_LOCATION}/boost.installed
+    COMMENT "Clean-up Boost")
+else()
   add_custom_command(
     OUTPUT ${CUSTOM_BOOST_LOCATION}/boost.cleanedup
     COMMAND ${CMAKE_COMMAND} -E rm -r ${BOOST_BUILD_DIR}

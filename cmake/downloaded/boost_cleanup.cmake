dd8b5a092df2e5da1ef1249c9f0730f882e4b641
Lori A. Burns <lori.burns@gmail.com>
2023-04-07 14:31:47 -0400

    right syntax

--- cmake/downloaded/boost_cleanup.cmake
+++ cmake/downloaded/boost_cleanup.cmake
@@ -2,13 +2,13 @@
 # licensed under BSD-3: https://github.com/coderefinery/autocmake/blob/master/LICENSE
 
 # Clean-up
-add_custom_command(
+if(NOT WIN32)
+  # observed trouble deleting the directory on Windows + MinGW + multiple builds
+  add_custom_command(
     OUTPUT ${CUSTOM_BOOST_LOCATION}/boost.cleanedup
-    if(NOT WIN32)
-        # observed trouble deleting the directory on Windows + MinGW + multiple builds
-        COMMAND ${CMAKE_COMMAND} -E rm -r ${BOOST_BUILD_DIR}
-    endif()
+    COMMAND ${CMAKE_COMMAND} -E rm -r ${BOOST_BUILD_DIR}
     COMMAND ${CMAKE_COMMAND} -E touch ${CUSTOM_BOOST_LOCATION}/boost.cleanedup
     WORKING_DIRECTORY ${CUSTOM_BOOST_LOCATION}
     DEPENDS ${CUSTOM_BOOST_LOCATION}/boost.installed
     COMMENT "Clean-up Boost")
+endif()

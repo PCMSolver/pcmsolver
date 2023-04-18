be2e5e1864cb44952327da9d7c64376bbb0d0b60
Lori A. Burns <lori.burns@gmail.com>
2023-04-18 18:21:57 -0400

    add licenses

--- tools/versioner.py
+++ tools/versioner.py
@@ -1,17 +1,30 @@
 #! /usr/bin/env python
 
-#
 # @BEGIN LICENSE
 #
-#   versioner.py: defines look-ahead auto-versioning from metadata.py
+# Psi4: an open-source quantum chemistry software package
 #
-#   Copyright (c) 2017 The Psi4 Developers
+# Copyright (c) 2007-2022 The Psi4 Developers.
 #
-#   All rights reserved. Use of this source code is governed by a
-#   BSD-style license that can be found in the LICENSE file.
+# The copyrights for code used from other parties are included in
+# the corresponding files.
 #
-# @END LICENSE
+# This file is part of PCMSolver.
 #
+# Psi4 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# Psi4 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with Psi4; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 from __future__ import print_function
 
 import argparse
